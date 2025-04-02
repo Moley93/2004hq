b62684d84803f3d98cbe7ad429541a664dce217c
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-04-02 14:50:08 +0100

    blackknight update

--- pages/questguides/free/blackknightsfortress.php
+++ pages/questguides/free/blackknightsfortress.php
@@ -25,7 +25,9 @@ Instructions:<br>
 <br>
 6. Go back downstairs to the main floor and push the wall. Now it's time to open the door on your east. One of the guards will say that the Black knights are having an important meeting and they will kill everyone who gets in. Say that you are brave and go in. One of the Black knights will attack you. Either kill him or go up the ladder. Walk east and south and push the wall. You will see a hole. Remember the Cabbage you picked? Use that Cabbage on the hole. The Cabbage will fall into the cauldron and destroy the weapon. Now your job is done.<br>
 <br>
-7. Go back and talk to Sir Amik Varze. He will reward you.
+7. Go back and talk to Sir Amik Varze. He will reward you.<br>
+<br>
+<img src="https://web.archive.org/web/20080730005344im_/http://www.runehq.com/image/quests/f2p/black_knight/qc.gif" height="200" width="200"><br>
 <br>
 <br>This quest guide was written on RuneHQ by Henry-x. Thanks to DNKevin, Weezy patgil2003, MarilynManson, Nitr021, Ozzy, and pokemama for corrections.<br>
 <br>This quest guide was entered into the database on Sat, Feb 07, 2004, at 10:08:20 PM by Chownuggs and CJH and was last updated on Sat, Feb 05, 2005, at 06:17:02 AM by nitro21.<br>
