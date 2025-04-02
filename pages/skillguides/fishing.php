fbcb3fa6ccd96a077cfaa3c563157476b9075b91
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-04-02 13:55:33 +0100

    image update fishing

--- pages/skillguides/fishing.php
+++ pages/skillguides/fishing.php
@@ -29,14 +29,14 @@ You can find below the guides to each of the different types of fishing you can 
 <tr>
 <td><center>Shrimp<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/shrimp.gif" width="48" height="32"></center></td>
 <td><center>Level 1<br>Type: Net Fishing</center></td>
-<td><center>Sardine<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/sardine.gif"></center></td>
+<td><center>Sardine<br><img src="https://www.itemdb.biz/images/icons/327.png"></center></td>
 <td><center>Level 5<br>Type: Sea bait fishing</center></td>
 </tr>
 
 
 <tr>
 <td><center>Karambwanji<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/karambwanji.gif"></center></td>
-<td><center>Level 5<br>Type: Net Fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html" class="c">Members only</a></center></td>
+<td><center>Level 5<br>Type: Net Fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html"></center></td>
 <td><center>Herring<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/herring.gif" width="48" height="32"></center></td>
 <td><center>Level 10<br>Type: Sea bait fishing</center></td>
 </tr>
@@ -46,7 +46,7 @@ You can find below the guides to each of the different types of fishing you can 
 <td><center>Anchovies<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/anchovy.gif"></center></td>
 <td><center>Level 15<br>Type: Net Fishing</center></td>
 <td valign="top"><center>Mackerel<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/mackerel.gif" width="48" height="32"></center></td>
-<td><center>Level 16<br>Type: Big net fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html" class="c">Members only</a></center></td>
+<td><center>Level 16<br>Type: Big net fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html"></center></td>
 </tr>
 
 
@@ -54,7 +54,7 @@ You can find below the guides to each of the different types of fishing you can 
 <td valign="top"><center>Trout<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/trout.gif" width="48" height="32"></center></td>
 <td><center>Level 20<br>Type: Fly Fishing</center></td>
 <td valign="top"><center>Cod<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/cod.gif" width="48" height="32"></center></td>
-<td><center>Level 23<br>Type: Big net fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html" class="c">Members only</a></center></td>
+<td><center>Level 23<br>Type: Big net fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html"></center></td>
 </tr>
 
 
@@ -62,7 +62,7 @@ You can find below the guides to each of the different types of fishing you can 
 <td valign="top"><center>Pike<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/pike.gif" width="48" height="32"></center></td>
 <td><center>Level 25<br>Type: River bait fishing</center></td>
 <td><center>Slimey Eel<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/slimeyeel.gif" width="48" height="32"></center></td>
-<td><center>Level 28<br>Type: River bait fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html" class="c">Members only</a></center></td>
+<td><center>Level 28<br>Type: River bait fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html"></center></td>
 </tr>
 
 
@@ -77,26 +77,26 @@ You can find below the guides to each of the different types of fishing you can 
 <td valign="top"><center>Lobster<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/lobster.gif" width="48" height="32"></center></td>
 <td><center>Level 40<br>Type: Lobster fishing</center></td>
 <td valign="top"><center>Bass<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/bass.gif" width="48" height="32"></center></td>
-<td><center>Level 46<br>Type: Big net fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html" class="c">Members only</a></center></td>
+<td><center>Level 46<br>Type: Big net fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html"></center></td>
 </tr>
 
 <tr>
 <td valign="top"><center>Swordfish<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/swordfish.gif" width="48" height="32"></center></td>
 <td><center>Level 50<br>Type: Harpoon fishing</center></td>
 <td valign="top"><center>Karambwan<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/karambwan.gif" width="48" height="32"></center></td>
-<td><center>Level 65<br>Type: Karambwan Vessel Fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html" class="c">Members only</a></center></td>
+<td><center>Level 65<br>Type: Karambwan Vessel Fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html"></center></td>
 </tr>
 
 <tr>
 <td valign="top"><center>Shark<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/shark.gif" width="48" height="32"></center></td>
-<td><center>Level 76<br>Type: Harpoon fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html" class="c">Members only</a></center></td>
+<td><center>Level 76<br>Type: Harpoon fishing<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html"></center></td>
 <td valign="top"><center>Sea Turtle<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/seaturtle.gif" width="48" height="32"></center></td>
-<td><center>Level 79<br>Type: Trawler Mini Game<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html" class="c">Members only</a></center></td>
+<td><center>Level 79<br>Type: Trawler Mini Game<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html"></center></td>
 </tr>
 
 <tr>
-<td valign="top"><center>Manta Ray<br><img src="https://web.archive.org/web/20050204055533im_/http://www.runescape.com/img/rs2/manual/fishing/mantaray.gif" width="48" height="32"></center></td>
-<td><center>Level 81<br>Type: Trawler Mini Game<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html" class="c">Members only</a></center></td>
+<td valign="top"><center>Manta Ray<br><img src="https://www.itemdb.biz/images/icons/389.png" width="48" height="32"></center></td>
+<td><center>Level 81<br>Type: Trawler Mini Game<br><a href="https://web.archive.org/web/20050204055533/http://www.runescape.com/aff/runescape/members/members.html"></center></td>
 </tr>
 <td valign="top"></td>
 <td></td>
