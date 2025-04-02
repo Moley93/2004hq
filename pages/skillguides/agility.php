37398090dbe04176c2af6f5f931965ce330c73e8
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-04-02 09:20:38 +0100

    agility initial test

---
+++ pages/skillguides/agility.php
@@ -0,0 +1,39 @@
+<table width="500" bgcolor="black" cellpadding="4" border="0"><tr><td class="e">
+<center><b>Agility guide</b> - (<a href="/web/20040606063634/http://www.runescape.com/aff/runescape/members/members.html" class="c"><i>members skill</i></a>)</center>
+<p>
+
+<i>Agility is only available to RuneScape <a href="/web/20040606063634/http://www.runescape.com/aff/runescape/members/members.html" class="c">members</a>.
+Please <a href="/web/20040606063634/http://www.runescape.com/aff/runescape/members/members.html" class="c">subscribe</a> to get this feature.</i>
+<p>
+
+<table><tr><td>
+Agility is a skill that can be used to get over, up and through various obstacles, 
+often as a short cut, or to get into new areas. 
+<p>
+Typical examples of where you use your agility skill are to squeeze through a pipe,
+swing on a rope over a hole, or use some handholds to climb up a wall.
+<p>
+Just click on the obstacle you wish to get past, and if you have the appropriate agility skill level you will attempt it.
+</td><td>
+<img src="https://web.archive.org/web/20040606063634im_/http://www.runescape.com/img/rs2/manual/logbalance.jpg" width="250" height="250">
+</td></tr></table>
+You will never fail some obstacles once you reach the appropriate agility level.
+On other obstacles it is possible to fall off, which will cause you to lose hitpoints.
+
+<p>
+A good way to advance your agility skill is to use the obstacle courses. 
+By going round the obstacle course in the correct order you can get an agility experience bonus from the course.
+This is in addition to the normal agility skill experience you get from attempting single obstacles.
+<p><table><tr><td>
+<br>
+Players with low-levels of ability might want to use the obstacle course in the tree gnome stronghold.
+<br><br>
+At level 35 you can also enter the obstacle course at the barbarian outpost.
+<br><br>
+There is also a high level obstacle course at the very north of the wilderness.
+</td><td>
+<img src="https://web.archive.org/web/20040606063634im_/http://www.runescape.com/img/rs2/manual/logbalance.jpg" width="250" height="250">
+</td></tr></table>
+<br>
+</td></tr></table>
+<br>
