f85a6ef619196cb05dd966f0bc2e368656710b9b
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-04-04 08:11:49 +0100

    droptable / calculator coming soon

--- pages/main/main.php
+++ pages/main/main.php
@@ -41,4 +41,43 @@ include('template/body.php');
         </td>
     </tr>
  </tbody>
+ <tbody>
+    <tr valign="top">
+        <td width="100" align="center">
+            <a href="?p=questlist">
+                <img src="kbdicon.webp" border="0">
+            </a>
+        </td>
+        <td width="120">
+            <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
+                <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;" />
+            </div>
+            <br />
+            <br />
+            2004 Drop Tables
+            <br />
+            <a href="" class="c">Coming Soon</a>
+        </td>
+
+        <td width="10"></td>
+        
+        <td width="100" align="center">
+            <a href="?p=skilllist">
+                <img src="swordicon.png" border="0">
+            </a>
+        </td>
+        
+        <td width="120">
+            <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
+                <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;" />
+            </div>
+            <br />
+            <br />
+            2004 Combat & Skill Calculators
+            <br />
+            <a href="" class="c">Coming Soon</a>
+        </td>
+    </tr>
+ </tbody>
  </table>
+
