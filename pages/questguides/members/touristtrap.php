e67996dd2e969696c12f086ef19e9109fe4fb2c9
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-04-07 09:38:53 +0100

    tourist trap added

---
+++ pages/questguides/members/touristtrap.php
@@ -0,0 +1,76 @@
+<?php
+function getPageContent() { return <<<HTML
+<b>2004HQ - Observatory Quest</b><br>
+<br>
+<b>Difficulty Level: <font color="Red">Hard</font></b><br>
+<br>
+<b>Quest Length: <font color="Red">Long</font></b><br>
+<br>
+Levels Required: 10 Fletching & 20 Smithing. (High-lvl Woodcutting helps on Cactus.)<br>
+<br>
+Items Required: Desert Gear [Boots, Robe, & Shirt], Shantay Pass, Knife, Full or Empty Waterskin(s), Hammer, 2-3 Bronze Bars, 10-30 Feathers, at least 105gp, Food, Amulet, and Weapon.(NOTE: The only armor that wont burn you in the desert is helmet, cape, and shield. But you MUST un-equip the helmet and shield around guards.)<br>
+<br>
+Rewards: Ana will give you a Wrought Iron Key useful for getting to a Mith & Addy mine in the Mining Camp. Irena will give you the choice of free exp in 2 skills - Agility, Thieving, Smithing, or Fletching. The higher your level the better the exp. Also, you can choose the same skill twice!, and the ability to make darts.<br>
+<br>
+1. Start by talking to Irena. She is found through the Shantay Pass. You can pay Shantay 5gp to buy a pass first. Then go through and speak with Irena. (NOTE Shantay does sell other items you will need for this quest.)<br>
+<br>
+<br>
+2. After Irena tells you about her daughter Ana, and leaves you to find her yourself. Go South a ways and examine the footsteps. They hint at you to go south (westward just a pinch, too) to the Mining Camp. (MAKE SURE you find the Kharidian Cactus & cut them to keep water in your Waterskin[s].)<br>
+<br>
+<br>
+3. Talk to the Mercenaries around there until you get them to admit that they don’t like the Captain. This will cost you 5gp a bribe. Find out that the Captain doesn’t like to fight his own battles. (IF you get busted they will take you to the desert and take all but ONE Waterskin.)<br>
+<br>
+<br>
+4. Now talk to the Captain, get him roused until he tells you he will call his Guards. Pick the option about asking him to fight his own fights. Then he challenges you. Once he’s dead a Metal Key will be added to your inventory. Use this to go inside with NO Armour or Weapons On! (ONCE you’ve killed the Captain you can come here to kill him again and get the key without all the dialogue.)<br>
+<br>
+<br>
+5. Talk to the Male Slave who’s trying to escape. After some conversation trade clothes with him. You will have to speak to him a second time to do so. (IF you get caught inside the camp the Guards & Mercenaries may throw you in jail. A Cell Key can be found upstairs in Captain Siad’s desk.)<br>
+<br>
+<br>
+6. Now with your slave outfit on go downstairs through the huge door and follow the path to the TWO Mercenaries. Talk to one and ask him to let you trade spots. He will hint at you getting a Pineapple for him first. (MAKE SURE you don’t have armor or weapons on around the Mercenaries.)<br>
+<br>
+7. Leave the camp completely. Go due west until you find the Nomad’s Camp. Talk to Al Shabim until you can ask him for a Pineapple. He will reveal the plan to get Blueprints from Captain Siad. Agree and he will give you a copy of the Chest Key. Now head back to the Mining Camp. (NOTE you can get more supplies from the nomads.)<br>
+<br>
+<br>
+8. Inside the Mining Camp go upstairs in the building to Captain Siad’s room. Get the Cell Key by searching his desk. Search the Bookcases till you find that he has books about sailing. Now talk to him.<br>
+<br>
+<br>
+9. Start with “Hello”. Try to continue talking to him and then start on sailing. After a while choose the comment on his “Jib”. He will then be distracted enough to unlock the Chest and get the Blueprints.<br>
+<br>
+<br>
+10. Bring these back to Al Shabim and he will ask you to make the weapon from them. Go north a bit and talk to the Bebadin Guard there. He will let you in the secret room to use the Experimental Anvil. Make the weapon; it is a dart tip. You may fail once or twice. Once the tip is made use feathers with it until you can get a complete Experimental Dart. (NOTE you may need to leave through the Shantay Pass and get more supplies.)<br>
+<br>
+<br>
+11. Take this to Al Shabim and he will give you the Pineapple and Six Darts. You now also have the ability to make DARTS! Go back to the Mining Camp.<br>
+<br>
+<br>
+12. Go through the gates with NO Armor or Weapons equipped. Then down to the mines. Give the Pineapple to the Mercenary and go through the Mine Cave.<br>
+<br>
+<br>
+13. Close to the mine cart is a bunch of Barrels. Search an empty one and take it. Now get in the mine cart. This may take a few tries depending on your agility level. (MAKE SURE you have food because you will get hurt.)<br>
+<br>
+<br>
+14. Once the cart arrives on the other side go northwest a bit till you find a large area with many Slaves. Ana will be there. If she asks how you got in tell her the line about it being secretive. (DO NOT SAY YOU HAVE A KEY! If you do you will be rounded up. To get out you will have to mine 15 rocks and give to the Mercenary.)<br>
+<br>
+<br>
+15. After talking to Ana use the Barrel with her to “kidnap” her. Place her in the cart then get in it yourself.<br>
+<br>
+<br>
+16. On the other side search the Barrels till you find Ana. Go to the Winch Bucket and use it. The Guard there will ask to help you. Agree. Ana will start complaining but you have to cover for her. Pick the statements that will compliment the Guard. (DO NOT attempt to walk through the Mine Cave, you will be caught and punished SEVERLY.)<br>
+<br>
+<br>
+17. Leave the mines and go over to the Winch above ground. Use it and then search the Barrels for Ana. Use her on the Mine Cart and place her in it. (DO NOT try to walk through the Gates, you will be caught and SEVERLY punished.)<br>
+<br>
+<br>
+18. Now say “Nice Cart” to the Mine Cart Driver and select all the lines that are nice or “funny”. Some of them will be stupid. Make him laugh and he will agree to help you escape. You will have to bribe him. Pay him 100gp and he will do it. Get in the cart and he will go. 
+<br>
+<br>
+19. Once outside go north to Irena and talk to her.<br>
+<br>
+<img src="img\questimages\ttrap1.png" height="200" width="350"><br>
+<br>
+This quest guide was written by imthecoolest, and haxhi4life. Thanks to DRAVAN for corrections.<br>
+
+This quest guide was entered into the database on Wed, Jun 09, 2004, at 12:46:01 PM by DRAVAN and CJH and was last updated on Wed, Dec 01, 2004, at 11:20:10 PM by DRAVAN.<br>
+
+HTML; } ?>
