ef1c26a61e1f25a858237da8bfd5f2d6520100aa
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-04-07 21:32:23 +0100

    footer discord add

--- template/footer.php
+++ template/footer.php
@@ -19,8 +19,8 @@
                                             <td align="bottom"><img src="img\edge_g2.jpg" width="100" height="82" hspace="0" vspace="0"></td>
                                             <td align="bottom">
                                                 <div align="center" style="font-size:16px;height:40px;" >
-                                                    Site by <a href="mailto:moley@2004hq.com">Moley</a>, <a href="mailto:eilert@2004hq.com">Eilert</a>,  <a href="mailto:halogod@2004hq.com">Halogod</a>, and <a href="mailto:collin@2004hq.com">Collin</a><br />
-                                                    Fan project - Not affiliated with Lost City.<br />
+                                                    Site by Moley, Eilert,  Halogod, and Collin<br />
+                                                    Fan project - Not affiliated with Lost City. Join our <a href="https://discord.gg/jdFmMvz33j">Discord<br />
                                                 </div>
                                                 <img src="img\edge_c.jpg" width="400" height="42" hspace="0" vspace="0">
                                             </td>
