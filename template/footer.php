7af81d00cd37178d6962974e1143037f9f66d57e
Collin <Collin@MineCove.net>
2025-04-07 04:44:44 -0500

    added missing line break in footer

--- template/footer.php
+++ template/footer.php
@@ -1,31 +1,38 @@
-               <br />
-             </center>
-         </td>
-         <td width="7"></td>
-     </tr>
- </tbody></table>
- <table cellpadding="0" cellspacing="0"><tbody><tr><td colspan="3"><img src="img\fm_bottom.gif"></td></tr></tbody></table>
-<br>
-                <br>
-            </center>
-        </td>
-    </tr>
-</tbody></table>
-<table cellpadding="0" cellspacing="0">
-    <tbody><tr>
-        <td align="bottom"><img src="img\edge_g2.jpg" width="100" height="82" hspace="0" vspace="0"></td>
-        <td align="bottom">
-            <div align="center" style="font-family:Arial,Helvetica,sans-serif; font-size:11px;">
-                                    Site by <a href="mailto:moley@2004hq.com">Moley</a>, <a href="mailto:eilert@2004hq.com">Eilert</a>, and <a href="mailto:collin@2004hq.com">Collin</a><br />
-                                    Fan project - Not affiliated with Lost City.<br>
-                                           </div>
-            <img src="img\edge_c.jpg" width="400" height="42" hspace="0" vspace="0">
-        </td>
-        <td align="bottom"><img src="img\edge_h2.jpg" width="100" height="82" hspace="0" vspace="0"></td>
-    </tr>
-</tbody></table>
-</div></center></td>
-</tr>
-</tbody></table>
-
-</body></html>
+                                                                    <br />
+                                                                    </center>
+                                                                </td>
+                                                                <td width="7"></td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <table cellpadding="0" cellspacing="0"><tbody><tr><td colspan="3"><img src="img\fm_bottom.gif"></td></tr></tbody></table>
+                                                    <br>
+                                                    <br>
+                                                </center>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <table cellpadding="0" cellspacing="0">
+                                    <tbody>
+                                        <tr>
+                                            <td align="bottom"><img src="img\edge_g2.jpg" width="100" height="82" hspace="0" vspace="0"></td>
+                                            <td align="bottom">
+                                                <div align="center" style="font-family:Arial,Helvetica,sans-serif; font-size:11px;">
+                                                    Site by <a href="mailto:moley@2004hq.com">Moley</a>, <a href="mailto:eilert@2004hq.com">Eilert</a>, Halogod35, and <a href="mailto:collin@2004hq.com">Collin</a><br />
+                                                    Fan project - Not affiliated with Lost City.<br /><br />
+                                                </div>
+                                                <img src="img\edge_c.jpg" width="400" height="42" hspace="0" vspace="0">
+                                            </td>
+                                            <td align="bottom"><img src="img\edge_h2.jpg" width="100" height="82" hspace="0" vspace="0"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </center>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
