57b90ad51dad673067d567e2bbe78121d0b25248
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-04-07 21:36:29 +0100

    footer fix

--- template/footer.php
+++ template/footer.php
@@ -21,7 +21,7 @@
                                                 <div align="center" style="font-size:16px;height:40px;" >
                                                     Site by Moley, Eilert,  Halogod, and Collin<br />
                                                     Fan project - Not affiliated with Lost City.<br />
-                                                    Join our <a href="https://discord.gg/jdFmMvz33j">Discord<br />
+                                                    Join our <a href="https://discord.gg/jdFmMvz33j">Discord</a><br />
                                                 </div>
                                                 <img src="img\edge_c.jpg" width="400" height="42" hspace="0" vspace="0">
                                             </td>
