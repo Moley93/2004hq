10d072f71dac2c26491504c52c11dfe0c3796026
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-04-07 18:49:40 +0100

    halogod link

--- template/footer.php
+++ template/footer.php
@@ -19,7 +19,7 @@
                                             <td align="bottom"><img src="img\edge_g2.jpg" width="100" height="82" hspace="0" vspace="0"></td>
                                             <td align="bottom">
                                                 <div align="center" style="font-size:16px;height:40px;" >
-                                                    Site by <a href="mailto:moley@2004hq.com">Moley</a>, <a href="mailto:eilert@2004hq.com">Eilert</a>, Halogod35, and <a href="mailto:collin@2004hq.com">Collin</a><br />
+                                                    Site by <a href="mailto:moley@2004hq.com">Moley</a>, <a href="mailto:eilert@2004hq.com">Eilert</a>,  <a href="mailto:halogod@2004hq.com">Halogod</a>, and <a href="mailto:collin@2004hq.com">Collin</a><br />
                                                     Fan project - Not affiliated with Lost City.<br />
                                                 </div>
                                                 <img src="img\edge_c.jpg" width="400" height="42" hspace="0" vspace="0">
