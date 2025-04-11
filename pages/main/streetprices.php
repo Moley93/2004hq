2fa0672df854d090e6e04bfd6891f781d8a7f358
Collin_M <collin.marceaux@gmail.com>
2025-04-11 14:21:40 -0500

    fix meta data

--- pages/main/streetprices.php
+++ pages/main/streetprices.php
@@ -2,13 +2,15 @@
 
 function getPageContent() {
     global $meta_data;
-    $meta_data['title'] = 'Clue Tables';
+    $meta_data['title'] = 'Street Prices';
     $meta_data['og:title'] = $meta_data['title'];
-    $meta_data['og:url'] = '?p=cluetables';
-    $meta_data['og:image'] = 'img/clueicon.png';
+    $meta_data['og:url'] = '?p=streetprices';
+    $meta_data['og:image'] = 'img/rune_kite.png';
 
     return <<<HTML
-    Item Street prices per <a href="https://docs.google.com/spreadsheets/d/1kF10Oh29B3_pFw8Cq7cBRe119eGXcX8QJR22_Cf1qEE/?gid=0" target="_blank">Dead Botting's google spreadsheet</a>:<br><br>
+    Street Prices per <a href="https://docs.google.com/spreadsheets/d/1kF10Oh29B3_pFw8Cq7cBRe119eGXcX8QJR22_Cf1qEE/?gid=0" target="_blank">Dead Botting's google spreadsheet</a>:
+    <p style="font-style:italic;">This spreadsheet is designed as a reference for what prices to expect for items in the 2004scape Market.
+      None of this data should be considered 100% correct, and will be forever changing.</p>
 <div id="sheet-table">Loading...</div>
 
 <script type="module">
