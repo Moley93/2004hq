60c2d19ca26d58f584294ed09dcd6e8a7e4abf92
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-04-11 08:49:44 +0100

    defence fix

--- pages/skillguides/defence.php
+++ pages/skillguides/defence.php
@@ -6,66 +6,64 @@ function getSkillContent() { return <<<HTML
 <br>
 <br><br><b>Helmets</b><br><table style="text-align: center;" align="center">
 <tr>
-<td style="width: 450px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/allhelms.gif" alt="[image]"></td>
+<td style="width: 425px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/allhelms.gif" alt="[image]"></td>
 </tr>
 <tr>
 <td>There are two types of helmet: full helms and medium helms. Full helms offer better protection than medium helms, but casting magic or using a ranged weapon is more difficult when using a full helm, since your vision is partially obstructed.</td>
 </tr>
 </table><br><br><b>Chainmail</b><br><table style="text-align: center;" align="center">
 <tr>
-<td style="width: 450px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/allchains.gif" alt="[image]"></td>
+<td style="width: 425px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/allchains.gif" alt="[image]"></td>
 </tr>
 <tr>
 <td>Chainmail is made up of small rings connected together. It is particularly useful for deflecting slashing weapons and absorbing the impact of crush attacks. Chainmail is weak against stabbing weapons as small pointy blades can pierce the rings.  Chainmail is quite flexible and so does not cause any disadvantages while using ranged weapons. Mages will tend to find the armour gets slightly in the way of some of the finer spell casting techniques.</td>
 </tr>
 </table><br><br><b>Plate Armour</b><br><table style="text-align: center;" align="center">
 <tr>
-<td style="width: 450px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/allplates.gif" alt="[image]"></td>
+<td style="width: 425px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/allplates.gif" alt="[image]"></td>
 </tr>
 <tr>
 <td>Plate armour is much heavier and stronger than chain. Rangers and mages find it incredibly difficult to use plate armour in conjunction with firing arrows or casting spells.<br><br>Plate armour is weakest against crushing damage as it cannot absorb the impact as well as it can absorb slashing or stabbing attacks.</td>
 </tr>
 </table><br><br><b>Shields</b><br><table style="text-align: center;" align="center">
 <tr>
-<td style="width: 450px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/allshields.gif" alt="[image]"></td>
+<td style="width: 425px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/allshields.gif" alt="[image]"></td>
 </tr>
 <tr>
-<td>Both varieties of shield feature excellent defences against slashing attacks. However, the kite shield is weak against stabbing attacks, and, the square shield is weak against crush attacks. Using the shield while casting magic or ranging is very difficult as it restricts the user's movement.<br><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/crystalshield.gif" alt="[image]"><br>The crystal shield can only be used once you have completed the <a href="viewarticle.ws?article_id=1784">Roving Elves</a> quest and have 50 Agility. The crystal shield is especially good at defending ranged attacks, but provides little protection from magic. As the shield protects you from damage, it will weaken, until it eventually turns back into the crystal seed from which it was grown.</td>
-</tr><tr>
 <tr>
 <td><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/dragonbreathshield.gif" alt="[image]"><br>Invaluable in the fight against all fire-breathing dragons, this shield reduces the damage from dragon fire considerably, and is strongly recommended every time you fight dragons.</td>
 </tr>
 </table><br><br><b>Prayer Enhancing Armour</b><br><table style="text-align: center;" align="center">
 <tr>
-<td style="width: 450px;"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/items/clothing/monktp.gif" alt="[image]"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/initbody.gif" alt="[image]"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/barrows/items/verac_body.gif" alt="[image]"></td>
+<td style="width: 425px;"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/items/clothing/monktp.gif" alt="[image]"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/melee/initbody.gif" alt="[image]"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/barrows/items/verac_body.gif" alt="[image]"></td>
 </tr>
 <tr>
 <td>There are many items of armour in game that will boost prayer. With the support of the Gods, you can wear these items to ensure your prayer will be maintained for a longer amount of time. Such items include the Priest, Monk, Shade, Druid and Zamorak robe sets, the Initiate armour sets, and Verac the Defiled's armour.</td>
 </tr>
 </table><br><br><h4 style="display: inline;">Ranger Armour</h4><br><br><b>Leather</b><br><table style="text-align: center;" align="center">
 <tr>
-<td style="width: 450px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/ranged/allleather.gif" alt="[image]"></td>
+<td style="width: 425px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/ranged/allleather.gif" alt="[image]"></td>
 </tr>
 <tr>
 <td>Leather tends to be very weak armour, but can be improved by studding the leather or hardening it. Leather is more resistant to crush attacks and weaker against stab attacks. However, its flexibility makes it ideal for use by rangers.</td>
 </tr>
 </table><br><br><b>Dragonhide</b><br><table style="text-align: center;" align="center">
 <tr>
-<td style="width: 450px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/ranged/alldraghide.gif" alt="[image]"></td>
+<td style="width: 425px;"><img style="display: inline;display: block; margin: 0 auto 0 auto;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/ranged/alldraghide.gif" alt="[image]"></td>
 </tr>
 <tr>
 <td>The same applies for Dragonhide armour as to leather armour, although it is much stronger. The magic of dragons makes it very useful when defending against spells and magical effects. However, this tends to interfere with the wearer's ability to cast magic.</td>
 </tr>
 </table><br><br><h4 style="display: inline;">Mage Armour</h4><br><br><b>Mage Robes</b><br><table style="text-align: center;" align="center">
 <tr>
-<td style="width: 450px;"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/mage/magerobe.gif" alt="[image]"></td>
+<td style="width: 425px;"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/mage/magerobe.gif" alt="[image]"></td>
 </tr>
 <tr>
 <td>Mage robes offer no extra defence against ranged or melee combat, but do give an advantage for spell casting and defending against magic.</td>
 </tr>
 </table><br><br><b>Mystic Robes </b><br><table style="text-align: center;" align="center">
 <tr>
-<td style="width: 450px;"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/mage/mysticrobe.gif" alt="[image]"></td>
+<td style="width: 425px;"><img style="display: inline;" src="https://web.archive.org/web/20060702162501im_/http://www.runescape.com/img/kbase/armour/mage/mysticrobe.gif" alt="[image]"></td>
 </tr>
 <tr>
 <td>Mystic robes, like mage robes, offer no extra defence against ranged or melee combat. However, they give a bigger advantage to casting and defending against magic.</td>
