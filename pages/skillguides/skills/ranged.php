c1e653b8e5aff9251b0c189f36248984933f564c
Collin_M <collin.marceaux@gmail.com>
2025-04-13 14:48:44 -0500

    ranged guide fixed

--- pages/skillguides/skills/ranged.php
+++ pages/skillguides/skills/ranged.php
@@ -24,208 +24,229 @@ function getSkillContent($skill) { return <<<HTML
 
     <b>Ranging Guild</b>
     <br>
-    Experienced rangers (level 40) may enter the Ranging Guild in the Seers' Village, where they may practice shooting at targets and live foes of various levels. The Guild also offers a selection of items for sale, including a wide range of thrown weapons.
+    Experienced rangers (40) may enter the Ranging Guild in the Seers' Village, where they may practice shooting at targets and live foes of various levels. The Guild also offers a selection of items for sale, including a wide range of thrown weapons.
 
 
-    <b>Bow wield Requirements</b><br>
-    
-    <table bgcolor="black" cellpadding="6" width="360">
+    <h3>Bow wield Requirements</h3>
+    <table class="calculators">
     <tr>
-    <td width="70" bgcolor="#382418">Bow picture</td>
-    <td width="90" bgcolor="#382418">Bow Type</td>
-    <td width="70" bgcolor="#382418">Ranged level to wield</td>
+        <th>Bow Type</th>
+        <th>Ranged level</th>
+        <th>Arrow Types</th>
     </tr>
-
-
     <tr>
-    <td><canvas data-itemname="shortbow"></canvas><canvas data-itemname="longbow"></canvas></td>
-    <td>Standard</td>
-    <td>Level 1</td>
+        <td>
+            <canvas data-itemname="shortbow"></canvas>
+            <canvas data-itemname="longbow"></canvas><br>
+            Regular Bows
+        </td>
+        <td>1</td>
+        <td>
+            <canvas data-itemname="bronze_arrow_5"></canvas>
+            <canvas data-itemname="iron_arrow_5"></canvas><br>
+            Bronze & Iron
+        </td>
     </tr>
-
-
     <tr>
-    <td><canvas data-itemname="oak_shortbow"></canvas><canvas data-itemname="oak_longbow"></canvas></td>
-    <td>Oak</td>
-    <td>Level 5</td>
+        <td>
+            <canvas data-itemname="oak_shortbow"></canvas>
+            <canvas data-itemname="oak_longbow"></canvas><br>
+            Oak Bows
+        </td>
+        <td>5</td>
+        <td>
+            <canvas data-itemname="bronze_arrow_5"></canvas>
+            <canvas data-itemname="iron_arrow_5"></canvas>
+            <canvas data-itemname="steel_arrow_5"></canvas><br>
+            Bronze, Iron, & Steel
+        </td>
     </tr>
-
-
     <tr>
-    <td><canvas data-itemname="willow_shortbow"></canvas><canvas data-itemname="willow_longbow"></canvas></td>
-    <td>Willow</td>
-    <td>Level 20<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
+        <td>
+            <canvas data-itemname="willow_shortbow"></canvas>
+            <canvas data-itemname="willow_longbow"></canvas><br>
+            Willow Bows
+        </td>
+        <td>20</td>
+        <td>
+            <canvas data-itemname="bronze_arrow_5"></canvas>
+            <canvas data-itemname="iron_arrow_5"></canvas>
+            <canvas data-itemname="steel_arrow_5"></canvas>
+            <canvas data-itemname="mithril_arrow_5"></canvas><br>
+            All Arrows up to Mithril
+        </td>
     </tr>
-
-
     <tr>
-    <td><canvas data-itemname="maple_shortbow"></canvas><canvas data-itemname="maple_longbow"></canvas></td>
-    <td>Maple</td>
-    <td>Level 30<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
+        <td>
+            <canvas data-itemname="maple_shortbow"></canvas>
+            <canvas data-itemname="maple_longbow"></canvas><br>
+            Maple Bows
+        </td>
+        <td>30</td>
+        <td>
+            <canvas data-itemname="bronze_arrow_5"></canvas>
+            <canvas data-itemname="iron_arrow_5"></canvas>
+            <canvas data-itemname="steel_arrow_5"></canvas>
+            <canvas data-itemname="mithril_arrow_5"></canvas>
+            <canvas data-itemname="adamant_arrow_5"></canvas><br>
+            All Arrows up to Adamant
+        </td>
     </tr>
-
-
     <tr>
-    <td><canvas data-itemname="yew_shortbow"></canvas><canvas data-itemname="yew_longbow"></canvas></td>
-    <td>Yew</td>
-    <td>Level 40<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
+        <td>
+            <canvas data-itemname="yew_shortbow"></canvas>
+            <canvas data-itemname="yew_longbow"></canvas><br>
+            Yew Bows
+        </td>
+        <td>40</td>
+        <td>
+            <canvas data-itemname="bronze_arrow_5"></canvas>
+            <canvas data-itemname="iron_arrow_5"></canvas>
+            <canvas data-itemname="steel_arrow_5"></canvas>
+            <canvas data-itemname="mithril_arrow_5"></canvas>
+            <canvas data-itemname="adamant_arrow_5"></canvas>
+            <canvas data-itemname="rune_arrow_5"></canvas><br>
+            All Arrows up to Rune
+        </td>
     </tr>
-
-
     <tr>
-    <td><canvas data-itemname="magic_shortbow"></canvas><canvas data-itemname="magic_longbow"></canvas></td>
-    <td>Magic</td>
-    <td>Level 50<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
+        <td>
+            <canvas data-itemname="magic_shortbow"></canvas>
+            <canvas data-itemname="magic_longbow"></canvas><br>
+            Magic Bows
+        </td>
+        <td>50</td>
+        <td>
+            <canvas data-itemname="bronze_arrow_5"></canvas>
+            <canvas data-itemname="iron_arrow_5"></canvas>
+            <canvas data-itemname="steel_arrow_5"></canvas>
+            <canvas data-itemname="mithril_arrow_5"></canvas>
+            <canvas data-itemname="adamant_arrow_5"></canvas>
+            <canvas data-itemname="rune_arrow_5"></canvas><br>
+            All Arrows up to Rune
+        </td>
     </tr>
-
-
     </table>
+
+    <h3>Ranging Armour wield Requirements</h3>
     
-
-
-
-    <b>Ranging Armour wield Requirements</b><br>
-    
-    <table bgcolor="black" cellpadding="6" width="360">
-    <tr>
-    <td width="70" bgcolor="#382418">Armour picture</td>
-    <td width="90" bgcolor="#382418">Armour Type</td>
-    <td width="90" bgcolor="#382418">Ranged level to wield</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="leather_body"></canvas></td>
-    <td>Plain Leather Items</td>
-    <td>Level 1</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="hardleather_body"></canvas></td>
-    <td>Hard Leather Body</td>
-    <td>Level 1<br>(10 Defence)</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="studded_body"></canvas></td>
-    <td>Studded Leather Body</td>
-    <td>Level 20<br>(20 Defence)</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="studded_chaps"></canvas></td>
-    <td>Studded chaps</td>
-    <td>Level 20</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="coif"></canvas></td>
-    <td>Coif</td>
-    <td>Level 20</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="dragonhide_body"></canvas></td>
-    <td>Green Dragonhide body</td>
-    <td>Level 40</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="dragonhide_chaps"></canvas></td>
-    <td>Green Dragonhide chaps</td>
-    <td>Level 40</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="dragonhide_vamb"></canvas></td>
-    <td>Green Dragonhide vambraces</td>
-    <td>Level 40</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="trail_robin_hood_hat"></canvas></td>
-    <td>Robin Hood hat</td>
-    <td>Level 40<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="trail_ranger_boots"></canvas></td>
-    <td>Rangers Boots</td>
-    <td>Level 40<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="blue_dragonhide_body"></canvas></td>
-    <td>Blue Dragonhide body</td>
-    <td>Level 50<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="blue_dragonhide_chaps"></canvas></td>
-    <td>Blue Dragonhide chaps</td>
-    <td>Level 50<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="blue_dragonhide_vambraces"></canvas></td>
-    <td>Blue Dragonhide vambraces</td>
-    <td>Level 50<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="red_dragonhide_body"></canvas></td>
-    <td>Red Dragonhide body</td>
-    <td>Level 60<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="red_dragonhide_chaps"></canvas></td>
-    <td>Red Dragonhide chaps</td>
-    <td>Level 60<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="red_dragonhide_vambraces"></canvas></td>
-    <td>Red Dragonhide vambraces</td>
-    <td>Level 60<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="black_dragonhide_body"></canvas></td>
-    <td>Black Dragonhide body</td>
-    <td>Level 70<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="black_dragonhide_chaps"></canvas></td>
-    <td>Black Dragonhide chaps</td>
-    <td>Level 70<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
-
-    <tr>
-    <td><canvas data-itemname="black_dragonhide_vambraces"></canvas></td>
-    <td>Black Dragonhide vambraces</td>
-    <td>Level 70<br><a href="https://web.archive.org/web/20050204063837/http://www.runescape.com/aff/runescape/members/members.html"</td>
-    </tr>
-
+    <table class="calculators">
+        <tr>
+            <th>Armour Type</td>
+            <th>Ranged to wield</th>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="hardleather_body"></canvas><br>
+                Hard Leather Body
+            </td>
+            <td>1<br>(10 Defence)</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="leather_cowl"></canvas>
+                <canvas data-itemname="leather_body"></canvas>
+                <canvas data-itemname="leather_chaps"></canvas>
+                <canvas data-itemname="leather_vambraces"></canvas><br>
+                Cowl, Leather body, chaps, & vambraces
+            </td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="studded_body"></canvas><br>
+                Studded Leather Body
+            </td>
+            <td>20<br>(20 Defence)</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="studded_chaps"></canvas><br>
+                Studded chaps
+            </td>
+            <td>20</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="coif"></canvas><br>
+                Coif
+            </td>
+            <td>20</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="dragonhide_body"></canvas><br>
+                Green Dragonhide body
+            </td>
+            <td>40<br>(40 defence +<br>Dragon Slayer)</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="dragonhide_vamb"></canvas>
+                <canvas data-itemname="dragonhide_chaps"></canvas><br>
+                Green Dragonhide chaps & vambraces
+            </td>
+            <td>40</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="trail_robin_hood_hat"></canvas><br>
+                Robin Hood hat
+            </td>
+            <td>40</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="trail_ranger_boots"></canvas><br>
+                Rangers Boots
+            </td>
+            <td>40</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="blue_dragonhide_body"></canvas><br>
+                Blue Dragonhide body
+            </td>
+            <td>50<br>(40 defence)</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="blue_dragonhide_vambraces"></canvas>
+                <canvas data-itemname="blue_dragonhide_chaps"></canvas><br>
+                Blue Dragonhide chaps & vambraces
+            </td>
+            <td>50</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="red_dragonhide_body"></canvas><br>
+                Red Dragonhide body
+            </td>
+            <td>60<br>(40 defence)</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="red_dragonhide_vambraces"></canvas>
+                <canvas data-itemname="red_dragonhide_chaps"></canvas><br>
+                Red Dragonhide chaps & vambraces
+            </td>
+            <td>60</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="black_dragonhide_body"></canvas><br>
+                Black Dragonhide body
+            </td>
+            <td>70<br>(40 defence)</td>
+        </tr>
+        <tr>
+            <td>
+                <canvas data-itemname="black_dragonhide_chaps"></canvas>
+                <canvas data-itemname="black_dragonhide_vambraces"></canvas><br>
+                Black Dragonhide chaps & vambraces
+            </td>
+            <td>70</td>
+        </tr>
     </table>
     
 </p>
