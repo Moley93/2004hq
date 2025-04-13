1d840b96ff3f08bb7a244836b9adbb42e87a2efc
Collin_M <collin.marceaux@gmail.com>
2025-04-12 23:32:10 -0500

    rewrite test

--- index.php
+++ index.php
@@ -1,47 +1,62 @@
 <?php
 include 'config.php';
-if (!empty($_GET['p'])) {
-    switch (htmlspecialchars($_GET['p'])) {
-        case 'questguides':
-            include 'pages/questguides/main.php';
-            break;
-        case 'skillguides':
-            include 'pages/skillguides/main.php';
-            break;
-        case 'droptables':
-            include 'pages/droptables/main.php';
-            break;
-        case 'cluetables':
-            include 'pages/cluetables/main.php';
-            break;
-        case 'clueguides':
-            include 'pages/clueguides/main.php';
-            break;
-        case 'calculators':
-            include 'pages/calculators/main.php';
-            break;
-        //case 'updatetables':
-        //    include 'pages/api/updatecluetables.php';
-        //    break;
-        case 'streetprices':
-            include 'pages/streetprices/main.php';
-            break;
-        case 'changelog':
-            include 'pages/main/changelog.php';
-            break;
-        case '403':
-            include 'pages/main/error/403.php';
-            break;
-        default:
-        case '404':
-            include 'pages/main/error/404.php';
-            break;
-    }
-} else {
-    include 'pages/main/homepage.php';
+
+$p = htmlspecialchars($_GET['p'] ?? '');
+
+switch ($p) {
+    case 'questguides':
+        include 'pages/questguides/main.php';
+        break;
+
+    case 'skillguides':
+        include 'pages/skillguides/main.php';
+        break;
+
+    case 'droptables':
+        include 'pages/droptables/main.php';
+        break;
+
+    case 'cluetables':
+        include 'pages/cluetables/main.php';
+        break;
+
+    case 'clueguides':
+        include 'pages/clueguides/main.php';
+        break;
+
+    case 'calculators':
+        include 'pages/calculators/main.php';
+        break;
+
+    case 'streetprices':
+        include 'pages/streetprices/main.php';
+        break;
+
+    case 'changelog':
+        include 'pages/main/changelog.php';
+        break;
+
+    case '403':
+        include 'pages/main/error/403.php';
+        break;
+
+    case '404':
+        include 'pages/main/error/404.php';
+        break;
+
+    case '': // No ?p= value (homepage)
+        include 'pages/main/homepage.php';
+        break;
+
+    default: // All other unknown pages
+        include 'pages/main/error/404.php';
+        break;
 }
-if (!$stopload) {
+
+
+if (empty($stopload)) {
     $pageHTML = getPageContent();
+
     include 'template/header.php';
     include 'template/body.php';
     echo $pageHTML;
