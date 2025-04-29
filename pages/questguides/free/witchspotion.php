2b03229d1ec30562aba8cdaf851130da7a66816a
halogod35 <hondadavey@gmail.com>
2025-04-29 18:16:40 -0400

    fix: updated to use official descriptions from 2004 and new bullet style

--- pages/questguides/free/witchspotion.php
+++ pages/questguides/free/witchspotion.php
@@ -1,33 +1,36 @@
 <?php
 function getQuestGuide($questName, $questComplete) { return <<<HTML
 <h2>$questName</h2>
+<b>Description:</b> Become one with your darker side. Tap into your hidden depths of magical potential by making a potion with the help of Hetty the Rimmington witch.<br>
 <br>
-Description: Become one with your darker side. Tap into your hidden depths of magical potential by making a potion with the help of Hetty the Rimmington witch.<br>
-<br>
-<b>Difficulty Level: <font color="Green">Easy</font></b><br>
-<br>
+<b>Difficulty Level: <font color="Green">Easy</font></b>
+<br><br>
 <b>Length: <font color="Green">Short</font></b><br>
+<h3>Items & Skills Needed:</h3>
+<ul style="list-style-type: none;">
+<li><div data-progress><canvas data-itemname="burnt_meat" data-size="25" data-show-label="inline"></canvas></div><br></li>
+<li><div data-progress><canvas data-itemname="eye_of_newt" data-size="25"></canvas>Eye of newt (or 300 coins to purchase)</div><br></li>
+<li><div data-progress><canvas data-itemname="onion" data-size="25" data-show-label="inline"></canvas></div><br></li>
+</ul>
 <br>
-Items/Skills Needed To Start: <input type="checkbox"> 300 coins to buy an eye of newt<br>
+<b>Starting Location:</b> Rimmington
+<br><br>
+<b>Reward:</b> 1 quest point, 325 Magic XP
+<br><br>
+<hr>
+<h3>Instructions:</h3>
 <br>
-Starting Location: Rimmington<br>
-<br>
-Reward: 1 Quest Point, 275 Magic XP<br>
-<br>
-<b>Instructions:</b><br>
-<br>
-<input type="checkbox"> Talk to the witch Hetty, and she will tell you about getting the ingredient for the cauldron. You will need four items: an onion, burnt meat, an eye of newt, and a rat tail.<br>
-<br>
-<input type="checkbox"> An onion can be found northwest of Rimmington. When you find the place, pick one.<br>
-<br>
-<input type="checkbox"> Burnt meat: You will need either burnt rat meat or burnt bear meat. I would suggest rat meat. Just go east of the village and you will find some big rats. Kill one and get the meat. Return to the village. Go to the house north of Hetty's. Use the meat on the range. If you have cooked it successfully, use the cooked meat on the range again. You will get a burnt meat.<br>
-<br>
-<input type="checkbox"> Rat tail: Just kill one of the rats in this village, and it will drop a rat tail. You must start the quest before killing the rat.<br>
-<br>
-<input type="checkbox"> After getting the ingredients, return to Hetty's house and talk to her. She will tell you the cauldron is done and ask you to drink it. Click on the cauldron to drink it, and the quest is finished.<br>
+<div data-progress>Talk to the witch Hetty, and she will tell you about getting the ingredient for the cauldron. You will need four items: an onion, burnt meat, an eye of newt, and a rat tail.</div>
+<br><br>
+<div data-progress>An onion can be found northwest of Rimmington. When you find the place, pick one.</div>
+<br><br>
+<div data-progress>Burnt meat: You will need either burnt rat meat or burnt bear meat. I would suggest rat meat. Just go east of the village and you will find some big rats. Kill one and get the meat. Return to the village. Go to the house north of Hetty's. Use the meat on the range. If you have cooked it successfully, use the cooked meat on the range again. You will get a burnt meat.</div>
+<br><br>
+<div data-progress>Rat tail: Just kill one of the rats in this village, and it will drop a rat tail. You must start the quest before killing the rat.</div>
+<br><br>
+<div data-progress>After getting the ingredients, return to Hetty's house and talk to her. She will tell you the cauldron is done and ask you to drink it. Click on the cauldron to drink it, and the quest is finished.</div>
 $questComplete
-This quest guide was written on RuneHQ by Henry-X. Thanks to Weezy and Corruptus for corrections.<br>
-<br>
+This quest guide was written on RuneHQ by Henry-X. Thanks to Weezy and Corruptus for corrections.
+<br><br>
 This quest guide was entered into the database on Sat, Feb 07, 2004, at 12:00:31 PM by Chownuggs and was last updated on Thu, Apr 22, 2004, at 02:44:28 PM.
-<br>
 HTML; }
