86036d9087ab22c3b3cf7f42f3fbaa3d88ac58a2
halogod35 <hondadavey@gmail.com>
2025-05-01 18:06:07 -0400

    fix: update to new bullet style

--- pages/questguides/members/elementalworkshop.php
+++ pages/questguides/members/elementalworkshop.php
@@ -6,71 +6,79 @@ function getQuestGuide($questName, $questComplete) { return <<<HTML
 <b>Difficulty: <font color="Green">Novice</font></b>
 <br><br>
 <b>Length: <font color="Green">Short</font></b><br>
+<h3>Items & Skills Needed:</h3>
+<ul style="list-style-type: none;">
+<li><div data-progress>20 Mining</div><br></li>
+<li><div data-progress>20 Smithing</div><br></li>
+<li><div data-progress>20 Crafting</div><br></li>
+<li><div data-progress>A sharp object like a knife or sword</div><br></li>
+<li><div data-progress><canvas data-itemname="bronze_pickaxe" data-size="25"></canvas>Any pickaxe</div><br></li>
+<li><div data-progress><canvas data-itemname="leather" data-size="25" data-show-label="inline"></canvas></div><br></li>
+<li><div data-progress><canvas data-itemname="needle" data-size="25" data-show-label="inline"></canvas></div><br></li>
+<li><div data-progress><canvas data-itemname="thread" data-size="25" data-show-label="inline"></canvas></div><br></li>
+<li><div data-progress><canvas data-itemname="coal" data-size="25"></canvas>4 coal</div><br></li>
+<li><div data-progress><canvas data-itemname="hammer" data-size="25" data-show-label="inline"></canvas></div><br></li>
+</ul>
 <br>
-Requirements: 20 Mining, 20 Smithing, 20 Crafting<br>
+<b>Starting Location:</b> Search the bookcase on the east wall in the house southwest of the bank in Seers' Village
+<br><br>
+<b>Reward:</b> 1 quest point, 5,000 Crafting XP, 5,000 Smithing XP, access to the Elemental Workshop, Elemental Shield (+6 Magic Defence bonus), as well as the ability to make and wield them
+<br><br>
+<hr>
+<h3>Instructions:</h3>
 <br>
-Items Needed: <input type="checkbox"> A sharp object like a knife or sword, <input type="checkbox"> Pickaxe, <input type="checkbox"> Leather, <input type="checkbox"> Thread, <input type="checkbox"> Needle, <input type="checkbox"> 4 Coal, <input type="checkbox"> a Hammer<br>
-<br>
-Reward: 1 Quest Point, Elemental Shield (+6 Magic Defence bonus), 5,000 Crafting XP, 5,000 Smithing XP<br>
-<br>
-Start Point: Go to Seers' Village.<br>
-<br>
-<input type="checkbox"> To Start: Search the bookcase on the east wall in the house southwest of the bank in Seers' Village.<br>
-<br>
-<b>Instructions:</b><br>
-<br>
-<input type="checkbox"> Go to the house southwest of the bank in Seers' Village.<br>
-<br>
-<img src="img\questimages\elemental1.png" height="200" width="200"><br>
-<br>
-<input type="checkbox"> Search the bookcase on the east wall.<br>
-<br>
-<img src="img\questimages\elemental2.png" height="200" width="350"><br>
-<br>
-<input type="checkbox"> After you read the Battered Book, use your knife on the book and you will get a Battered Key. Do not drop the Battered Book — you will need it later to make the Elemental Shield.<br>
-<br>
-<input type="checkbox"> Go to the building north, which has an anvil in it, and use the key on the Odd Looking Wall. Then go down the stairs.<br>
-<br>
-<img src="img\questimages\elemental4.png" height="200" width="200"><br>
-<br>
-<input type="checkbox"> A map of the places for some of the next steps:<br>
-<br>
-<img src="img\questimages\elemental5.png" height="400" width="400"><br>
-<br>
-<input type="checkbox"> When you're in, go down the west path and try to mine a rock. You will be attacked by a level 35 Earth Elemental. Kill the Earth Elemental, and it will drop an Elemental Ore — pick it up.<br>
-<br>
-<img src="img\questimages\elemental6.png" height="200" width="200"><br>
-<br>
-<input type="checkbox"> Now go to the north passage from where you came in. Turn the valves so they are like this: First, you need to turn the right valve, then the left valve, to make the waterwheel move. Then pull the lever. (The right valve is the first, and the second is the left valve.)<br>
-<br>
-<img src="img\questimages\elemental7.png" height="200" width="350"><br>
-<br>
-<input type="checkbox"> After that, go to the east passage and fix the bellows. This is where the leather, thread, and needle come in.<br>
-<br>
-<img src="img\questimages\elemental8.png" height="200" width="400"><br>
-<br>
-<img src="img\questimages\elemental13.png" height="200" width="300"><br>
-<br>
-<input type="checkbox"> After that, search all the crates for a Stone Bowl (you only need to search each crate once). The location is different for each player. When you find it, go down the south passage.<br>
-<br>
-<img src="img\questimages\elemental9.png" height="200" width="350"><br>
-<br>
-<input type="checkbox"> Use the bowl on the lava, and you will get a Stone Bowl filled with lava. Then use the bowl with lava in it on the furnace.<br>
-<br>
-<img src="img\questimages\elemental10.gif" height="300" width="200"><br>
-<br>
-<input type="checkbox"> Go back to the bellows and pull the switch.<br>
-<br>
-<input type="checkbox"> Go back to the furnace and use your Elemental Ore and 4 Coal on the furnace.<br>
-<br>
-<img src="img\questimages\elemental11.png" height="200" width="300"><br>
-<br>
-<input type="checkbox"> Then go back to the center and use your Elemental Metal on the workbench. You will need to have the Battered Book in your inventory to make the shield, or it will tell you that you need instructions.<br>
-<br>
-<input type="checkbox"> When you've made the Elemental Shield, you've completed the quest.<br>
-Note: You can make more Elemental Shields after you've finished the quest.<br>
+<div data-progress>Go to the house southwest of the bank in Seers' Village.</div><br><br>
+<img src="img\questimages\elemental1.png" height="200" width="200">
+<br><br>
+<div data-progress>Search the bookcase on the east wall.</div>
+<br><br>
+<img src="img\questimages\elemental2.png" height="200" width="350">
+<br><br>
+<div data-progress>After you read the Battered Book, use your knife on the book and you will get a Battered Key. Do not drop the Battered Book — you will need it later to make the Elemental Shield.</div>
+<br><br>
+<div data-progress>Go to the building north, which has an anvil in it, and use the key on the Odd Looking Wall. Then go down the stairs.</div>
+<br><br>
+<img src="img\questimages\elemental4.png" height="200" width="200">
+<br><br>
+A map of the places for some of the next steps:
+<br><br>
+<img src="img\questimages\elemental5.png" height="400" width="400">
+<br><br>
+<div data-progress>When you're in, go down the west path and try to mine a rock. You will be attacked by a level 35 Earth Elemental. Kill the Earth Elemental, and it will drop an Elemental Ore — pick it up.</div>
+<br><br>
+<img src="img\questimages\elemental6.png" height="200" width="200">
+<br><br>
+<div data-progress>Now go to the north passage from where you came in. Turn the valves so they are like this: First, you need to turn the right valve, then the left valve, to make the waterwheel move. Then pull the lever. (The right valve is the first, and the second is the left valve.)</div>
+<br><br>
+<img src="img\questimages\elemental7.png" height="200" width="350">
+<br><br>
+<div data-progress>After that, go to the east passage and fix the bellows. This is where the leather, thread, and needle come in.</div>
+<br><br>
+<img src="img\questimages\elemental8.png" height="200" width="400">
+<br><br>
+<img src="img\questimages\elemental13.png" height="200" width="300">
+<br><br>
+<div data-progress>After that, search all the crates for a Stone Bowl (you only need to search each crate once). The location is different for each player. When you find it, go down the south passage.</div>
+<br><br>
+<img src="img\questimages\elemental9.png" height="200" width="350">
+<br><br>
+<div data-progress>Use the bowl on the lava, and you will get a Stone Bowl filled with lava. Then use the bowl with lava in it on the furnace.</div>
+<br><br>
+<img src="img\questimages\elemental10.gif" height="300" width="200">
+<br><br>
+<div data-progress>Go back to the bellows and pull the switch.</div>
+<br><br>
+<div data-progress>Go back to the furnace and use your Elemental Ore and 4 Coal on the furnace.</div>
+<br><br>
+<img src="img\questimages\elemental11.png" height="200" width="300">
+<br><br>
+<div data-progress>Then go back to the center and use your Elemental Metal on the workbench. You will need to have the Battered Book in your inventory to make the shield, or it will tell you that you need instructions.</div>
+<br><br>
+<div data-progress>When you've made the Elemental Shield, you've completed the quest.</div>
+<br><br>
+Note: You can make more Elemental Shields after you've finished the quest.
 $questComplete
-This quest guide was written by pj. Thanks to IglooGuy, Hagge, Angelinblack, Shinraz, villike, Nitr021, wackyax16, jdude720, my_mulisha, Xjezusx1, beaverboy40, jakesterwars, cafetero, and Im4eversmart for corrections.<br>
-<br>
-This quest guide was entered into the RuneHQ.com database on Wed, Jun 02, 2004, at 06:22:07 PM by Cav103 and CJH and was last updated on Mon, Oct 02, 2006, at 02:40:17 PM by Im4eversmart.<br>
+This quest guide was written by pj. Thanks to IglooGuy, Hagge, Angelinblack, Shinraz, villike, Nitr021, wackyax16, jdude720, my_mulisha, Xjezusx1, beaverboy40, jakesterwars, cafetero, and Im4eversmart for corrections.
+<br><br>
+This quest guide was entered into the RuneHQ.com database on Wed, Jun 02, 2004, at 06:22:07 PM by Cav103 and CJH and was last updated on Mon, Oct 02, 2006, at 02:40:17 PM by Im4eversmart.
 HTML; }
--- pages/questguides/members/familycrest.php
+++ pages/questguides/members/familycrest.php
@@ -27,14 +27,18 @@ function getQuestGuide($questName, $questComplete) { return <<<HTML
 <li><div data-progress><canvas data-itemname="waterrune" data-size="25"></canvas>x3 Water runes</div></li>
 <li><div data-progress><canvas data-itemname="earthrune" data-size="25"></canvas>x4 Earth runes</div></li>
 <li><div data-progress><canvas data-itemname="firerune" data-size="25"></canvas>x5 Fire runes</div></li>
-<li><div data-progress>Recommended: Armor, Weapon, Food & Antipoison for yourself</div></li>
 </ul>
-<br>
-<br>
-Starting Location: Southeast of the East Varrock bank<br>
-<br>
-Reward: 1 quest point, a pair of Steel Gauntlets<br>
-<br>
+<h3>Recommended:</h3>
+<ul style="list-style-type: none;">
+<li><div data-progress>Decent weapon and armor</div></li>
+<li><div data-progress>Food</div></li>
+<li><div data-progress>Antipoison</div></li>
+</ul>
+<br><br>
+<b>Starting Location:</b> Southeast of the East Varrock bank
+<br><br>
+<b>Reward:</b> 1 quest point, a pair of Steel Gauntlets
+<br><br>
 <hr>
 <h3>Starting Out:</h3>
 <br>
@@ -46,11 +50,9 @@ Reward: 1 quest point, a pair of Steel Gauntlets<br>
 <br>
 <hr>
 <h3>First Son:</h3>
-<img src="img/questimages/famcrest3.jpg" height="220" width="220"><img src="img/questimages/famcrest4.jpg" height="200" width="200"><br>
-<br>
-<br>
-<div data-progress>1. Talk to the Chef, who is located in a house just northeast of the Catherby bank. Talk to him, and he'll give you his piece of the crest
-    if you provide the following cooked fish. Note: If you don't have a high enough Cooking or Fishing level, you can buy the fish instead.</div>
+<img src="img/questimages/famcrest3.jpg" height="220" width="220"><img src="img/questimages/famcrest4.jpg" height="200" width="200">
+<br><br>
+<div data-progress>Talk to the Chef, who is located in a house just northeast of the Catherby bank. Talk to him, and he'll give you his piece of the crest if you provide the following cooked fish. Note: If you don't have a high enough Cooking or Fishing level, you can buy the fish instead.</div>
 <br>
 Shrimp<br>
 Salmon<br>
@@ -58,34 +60,24 @@ Tuna<br>
 Bass<br>
 Swordfish<br>
 <br>
-<div data-progress>2. Give him these fish and he will give you his piece of the crest. He should tell you about the second brother
-    in Al Kharid. If he doesn't, talk to him again and he will tell you to go to the Gem Trader in Al Kharid.</div>
+<div data-progress>Give him these fish and he will give you his piece of the crest. He should tell you about the second brother in Al Kharid. If he doesn't, talk to him again and he will tell you to go to the Gem Trader in Al Kharid.</div>
 <br>
 <hr>
 <h3>Second Son:</h3>
-<div data-progress>1. Talk to the Gem Trader in Al Kharid. He will tell you about a man with a poncey yellow cape who hangs around the gold rock
-    in the Scorpion Pit Chasm mine north of Al Kharid.</div>
+<div data-progress>Talk to the Gem Trader in Al Kharid. He will tell you about a man with a poncey yellow cape who hangs around the gold rock in the Scorpion Pit Chasm mine north of Al Kharid.</div>
 <br>
-<div data-progress>2. Talk to this Nobleman and he will tell you that if you want his part of the crest you will need to give him a ruby ring
-    and a ruby necklace made from the finest gold. He will tell you that a dwarf named Boot knows where to find the finest gold.</div>
+<div data-progress>Talk to this Nobleman and he will tell you that if you want his part of the crest you will need to give him a ruby ring and a ruby necklace made from the finest gold. He will tell you that a dwarf named Boot knows where to find the finest gold.</div>
 <br>
-<img src="img/questimages/famcrest5.jpg" height="220" width="220"><img src="img/questimages/famcrest6.jpg" height="200" width="200"><br>
+<img src="img/questimages/famcrest5.jpg" height="220" width="220"><img src="img/questimages/famcrest6.jpg" height="200" width="200">
+<br><br>
+<div data-progress>Go talk to Boot who is in the Dwarven Mine. See the world map if you can't find Dwarven Mine. (Hint: it's north of Falador, down the ladder in a house on the ice mountain.) Boot can be found by going southwest. He will tell you where the finest gold is found. Bring a pickaxe, good armor, a good weapon, and probably some food (you will need to defeat two level 122 Hellhounds).</div>
 <br>
-<div data-progress>3. Go talk to Boot who is in the Dwarven Mine. See the world map if you can't find Dwarven Mine. (Hint: it's north of
-    Falador, down the ladder in a house on the ice mountain.) Boot can be found by going southwest. He will tell you where the finest gold is
-    found. Bring a pickaxe, good armor, a good weapon, and probably some food (you will need to defeat two level 122 Hellhounds).</div>
-<br>
-<img src="img/questimages/famcrest7.jpg" height="220" width="220"><img src="img/questimages/famcrest9.jpg" height="200" width="200"><br>
-<br>
-<br>
-<div data-progress> 4. The finest gold is found in a dungeon east of Ardougne. To make this easy, go to East Ardougne and head southeast.
-    You will find a ladder surrounded by archways. Go down the ladder. First you will meet some hobgoblins (level 48.). You can't access the
-    finest gold directly; you'll need to pull several levers to activate the door. Go to where the door of the Hellhound is. Do the following
-    steps to unlock the door where the gold is:
+<img src="img/questimages/famcrest7.jpg" height="220" width="220"><img src="img/questimages/famcrest9.jpg" height="200" width="200">
+<br><br>
+<div data-progress>The finest gold is found in a dungeon east of Ardougne. To make this easy, go to East Ardougne and head southeast. You will find a ladder surrounded by archways. Go down the ladder. First you will meet some hobgoblins (level 48.). You can't access the finest gold directly; you'll need to pull several levers to activate the door. Go to where the door of the Hellhound is. Do the following steps to unlock the door where the gold is:
 </div>
-    Hint: If you don't know what a lever's status is (up or down) you can right click on it and click inspect. (Also, you may need to pull the
-    levers in the opposite order from what the guide says.)<br>
-<br>
+    Hint: If you don't know what a lever's status is (up or down) you can right click on it and click inspect. (Also, you may need to pull the levers in the opposite order from what the guide says.)
+<br><br>
 <div style="text-align:left;">
     <div data-progress>I) Go to the North wall and pull that lever up</div>
     <div data-progress>II) Back to the South room and pull that lever down</div>
@@ -93,66 +85,47 @@ Swordfish<br>
     <div data-progress>IV) Go inside the North room and pull that lever up</div>
     <div data-progress>V) Go outside of the room and pull the lever on the North wall up</div>
     <div data-progress>VI) Go back to the South room and pull that lever down</div>
-    <div data-progress>VII) Open the cage and mine 2 "perfect" ores. (Note: you may or may not have to kill them. Often times
-        there is already someone killing them for range practice.)</div>
+    <div data-progress>VII) Open the cage and mine 2 "perfect" ores. (Note: you may or may not have to kill them. Often times there is already someone killing them for range practice.)</div>
 </div>
 <br>
-<img src="img/questimages/famcrest10.jpg" height="220" width="220"><br>
+<img src="img/questimages/famcrest10.jpg" height="220" width="220">
+<br><br>
+<div data-progress>Use these gold nuggets on a furnace, and now you will have two gold bars. Get your two rubies, a ring mould, a necklace mould, and your two gold bars made from the finest gold. Use a bar on the furnace to make a ruby ring and a ruby necklace.</div>
 <br>
-<div data-progress>5. Use these gold nuggets on a furnace, and now you will have two gold bars. Get your two rubies,
-    a ring mould, a necklace mould, and your two gold bars made from the finest gold. Use a bar on the furnace to make a
-    ruby ring and a ruby necklace.</div>
-<br>
-<div data-progress>6. Return to Al Kharid with your ruby ring and ruby necklace made from the finest gold and give them
-    to the Nobleman. He will give you his piece and tell you about his younger brother.</div>
+<div data-progress>Return to Al Kharid with your ruby ring and ruby necklace made from the finest gold and give them to the Nobleman. He will give you his piece and tell you about his younger brother.</div>
 <br>
 <hr>
 <h3>Third Son:</h3>
 <br>
-<div data-progress>1. For this part you will need a cure poison potion (it's a good idea to bring two, one for the son and one for you),
-    probably some good armor, a good weapon, all of the runes needed to cast all four blast spells, and either a level 59 magic, or a Wizard's
-    Mind Bomb (can be bought in Falador Bar for 2 coins). By the way, a Wizard's Mind Bomb temporarily increases your Magic level by three,
-    so you can do this part if you have a level 56 magic or higher.</div>
+<div data-progress>For this part you will need a cure poison potion (it's a good idea to bring two, one for the son and one for you), probably some good armor, a good weapon, all of the runes needed to cast all four blast spells, and either a level 59 magic, or a Wizard's Mind Bomb (can be bought in Falador Bar for 2 coins). By the way, a Wizard's Mind Bomb temporarily increases your Magic level by three, so you can do this part if you have a level 56 magic or higher.</div>
 <br>
-<div data-progress>2. Once you have all of those items go to the Jolly Boar Inn. This is located northeast of Varrock. Check the world map
-    if you can't find it (it's the large bar). Go inside and go to the southern end. On the <span data-floors="US">2nd floor</span> you will
-    find, in a room, a wizard.</div>
+<div data-progress>Once you have all of those items go to the Jolly Boar Inn. This is located northeast of Varrock. Check the world map if you can't find it (it's the large bar). Go inside and go to the southern end. On the <span data-floors="US">2nd floor</span> you will find, in a room, a wizard.</div>
 <br>
 <img src="img/questimages/famcrest11.jpg" height="220" width="220"><img src="img/questimages/famcrest12.jpg" height="220" width="220"><br>
 <br>
-<div data-progress>3. He is the third son. Talk to him—he has been poisoned. Give him your cure poison potion and he will be cured. He will tell you all
-    about the poison spiders, and about Chronozon (121) having his piece of the crest. He will tell you that in order to kill Chronozon you will need
-    to cast all of the blast spells on him.</div><br>
-<br>
-<img src="img/questimages/edgedung.png" height="443" width="265"><br>
-<br>
-<div data-progress>4. Now get all of the items mentioned in step 1 and go to Edgeville. Enter the dungeon, and deep inside you'll find a gate that
-    can only be accessed on members' servers. (Examine the gates and eventually you will find one that says, "You can pass through this on members servers.")
-    This gate is protected by a couple of aggressive skeletons. The members only area is known as the 'Wilderness Dungeon'.</div><br>
-<br>
-<div data-progress>5. Go through the gate and you will first meet some thugs (level 10). Head west and get past the Chaos Druids (level 13). Go
-    north and get past the skeletons (level 45) by heading west into the narrow hallway.</div><br>
-<br>
-<div data-progress>6. You will need to get past Black Demons (level 172) and Poison Spiders (level 64). Once you feel ready, go west, then south.</div><br>
-<br>
-<div data-progress>7. Now you should be in a room with the Earth Obelisk and Chronozon (level 139). If he hasn't attacked you yet, initiate
-    the fight. While you are killing him, you must successfully cast all of the Blast spells, just once. If you don't, he will regenerate.
-    You can use prayers. Once you have killed him pick up the crest piece and get out! (Suggested method: Hide behind the pillar at the back
-    of the cage, eliminate any Poison Spiders that attack you, cure your poison, and begin casting the Blast spells on Chronozon. He will not
-    attack you from this position.)</div><br>
-<br>
-<img src="img/questimages/famcrest13.jpg" height="200" width="200"><br>
-<br>
+<div data-progress>He is the third son. Talk to him—he has been poisoned. Give him your cure poison potion and he will be cured. He will tell you all about the poison spiders, and about Chronozon (121) having his piece of the crest. He will tell you that in order to kill Chronozon you will need to cast all of the blast spells on him.</div>
+<br><br>
+<img src="img/questimages/edgedung.png" height="443" width="265">
+<br><br>
+<div data-progress>Now get all of the items mentioned in step 1 and go to Edgeville. Enter the dungeon, and deep inside you'll find a gate that can only be accessed on members' servers. (Examine the gates and eventually you will find one that says, "You can pass through this on members servers.") This gate is protected by a couple of aggressive skeletons. The members only area is known as the 'Wilderness Dungeon'.</div>
+<br><br>
+<div data-progress>Go through the gate and you will first meet some thugs (level 10). Head west and get past the Chaos Druids (level 13). Go north and get past the skeletons (level 45) by heading west into the narrow hallway.</div>
+<br><br>
+<div data-progress>You will need to get past Black Demons (level 172) and Poison Spiders (level 64). Once you feel ready, go west, then south.</div>
+<br><br>
+<div data-progress>Now you should be in a room with the Earth Obelisk and Chronozon (level 139). If he hasn't attacked you yet, initiate the fight. While you are killing him, you must successfully cast all of the Blast spells, just once. If you don't, he will regenerate. You can use prayers. Once you have killed him pick up the crest piece and get out! (Suggested method: Hide behind the pillar at the back of the cage, eliminate any Poison Spiders that attack you, cure your poison, and begin casting the Blast spells on Chronozon. He will not attack you from this position.)</div>
+<br><br>
+<img src="img/questimages/famcrest13.jpg" height="200" width="200">
+<br><br>
 <hr>
 <h3>Finishing Up:</h3>
-<div data-progress>Finishing the Quest: Now that you have all three pieces, put them together and go back and talk to Dimintheis.
-    He will be grateful and will reward you. You can have them enhanced—see below. Remember, you can only choose one enhancement,
-    so choose wisely.</div><br>
+<div data-progress>Finishing the Quest: Now that you have all three pieces, put them together and go back and talk to Dimintheis. He will be grateful and will reward you. You can have them enhanced—see below. Remember, you can only choose one enhancement, so choose wisely.</div>
+<br><br>
+Enhancing the Gauntlets: Go to any son and talk to him. He will enhance the gauntlets for you if you want.
 <br>
-Enhancing the Gauntlets: Go to any son and talk to him. He will enhance the gauntlets for you if you want.<br>
-<img src="img/questimages/famcrest11.png" height="150" width="450"><br>
+<img src="img/questimages/famcrest11.png" height="150" width="450">
 $questComplete
-This quest guide was written by Firkløver and gigakiller. Thanks to L3tHaL_LeAdA, Keystone, thehellkeeper, Your Homey 1, gypped, and DRAVAN for corrections.<br>
-<br>
-This quest guide was entered into the database on Tue, May 11, 2004, at 08:04:55 PM by Freakybat and CJH and was last updated on Sat, Aug 14, 2004, at 12:17:42 PM.<br>
+This quest guide was written by Firkløver and gigakiller. Thanks to L3tHaL_LeAdA, Keystone, thehellkeeper, Your Homey 1, gypped, and DRAVAN for corrections.
+<br><br>
+This quest guide was entered into the database on Tue, May 11, 2004, at 08:04:55 PM by Freakybat and CJH and was last updated on Sat, Aug 14, 2004, at 12:17:42 PM.
 HTML; }
