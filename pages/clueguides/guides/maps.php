bae6aa22375c2113523c99bdffe2677cf9938e37
Collin_M <collin.marceaux@gmail.com>
2025-05-14 19:12:26 -0500

    fix: update clue guides maps

--- pages/clueguides/guides/maps.php
+++ pages/clueguides/guides/maps.php
@@ -2,30 +2,53 @@
 function getGuideContent($guide) { return <<<HTML
 <h2>$guide Treasure Trails Guide</h2>
 <p>
-<table class="calculators">
+<table class="calculators" width="100%">
   <tr>
-    <th>Maps</th>
-    <th>Location</th>
+    <th colspan=2>Easy Clues</th>
   </tr>
   <tr> 
+    <td><img src="img/clue_guides/maps/easy/champ_guild.png" style="width:200px;" alt="champ_guild"></td>
+    <td>Champions Guild Map<br><br>West of Champions Guild</td>
+  </tr>
+  <tr> 
+    <td><img src="img/clue_guides/maps/easy/varrock_mine.png" style="width:200px;" alt="varrock_mine"></td>
+    <td>Varrock Mine Map<br><br>Dig at the fence at the Varrock East mining site</td>
+  </tr>
+  <tr> 
+    <td><img src="img/clue_guides/maps/easy/falador_mine.png" style="width:200px;" alt="falador_mine"></td>
+    <td>Falador Useless Rock Area Map<br><br>Fenced area East-NorthEast of Falador's North entrance, full of useless rocks.</td>
+  </tr>
+</table>
+<hr>
+<table class="calculators" width="100%">
+  <tr>
+    <th colspan=2>Medium Clues</th>
+  </tr>
+  <tr> 
+    <td><img src="img/clue_guides/maps/medium/draynor_fishing.png" style="width:200px;" alt="dranyor_fishing"></td>
+    <td>Draynor Fish Spot Map<br><br>South from Draynor's Bank</td>
+  </tr>
+  <tr> 
+    <td><img src="img/clue_guides/maps/medium/observatory.png" style="width:200px;" alt="observatory"></td>
+    <td>Obvseratory Map<br><br>West of Tree Gnome Village</td>
+  </tr>
+</table>
+<hr>
+<table class="calculators" width="100%">
+  <tr>
+    <th colspan=2>Hard Clues</th>
+  </tr>
+  <tr> 
+    <td><img src="img/clue_guides/maps/hard/varrock_lumber.png" style="width:200px;" alt="varrock_lumber.png"></td>
+    <td>Varrock Lumberyard Map<br><br>Northeast from Varrock</td>
+  </tr>
+  <!--<tr> 
     <td><img src="img/clue_guides/clue_black_warriors_fortress_wildy.png" style="width:200px;" alt="clue_black_warriors_fortress_wildy.png"></td>
     <td>Black warriors fortress in the Wilderness. Search the crate in the southwest corner of the area inside.</td>
   </tr>
  <tr> 
     <td><img src="img/clue_guides/clue_brother_galahad.jpg" style="width:200px;" alt="clue_brother_galahad.jpg"></td>
     <td>Brother Galahad's house across the river from Coal Trucks.</td>
-  </tr>
-  <tr> 
-    <td><img src="img/clue_guides/champ_guild.jpg" style="width:200px;" alt="champ_guild.jpg"></td>
-    <td>Champions Guild Map - West of Champions Guild</td>
-  </tr>
-  <tr> 
-    <td><img src="img/clue_guides/falador_mine.jpg" style="width:200px;" alt="falador_mine.jpg"></td>
-    <td>Falador Useless Rock Area Map - Fenced area East-NorthEast of Falador's North entrance, full of useless rocks.</td>
-  </tr>
-  <tr> 
-    <td><img src="img/clue_guides/dranor_fishing.jpg" style="width:200px;" alt="dranor_fishing.jpg"></td>
-    <td>Draynor Fish Spot Map - South from Draynor's Bank</td>
   </tr>
   <tr> 
     <td><img src="img/clue_guides/clue_goblins_near_observatory.jpg" style="width:200px;" alt="clue_goblins_near_observatory.jpg"></td>
@@ -36,47 +59,42 @@ function getGuideContent($guide) { return <<<HTML
     <td>At the Necromancer peninsula south of East Ardougne. 2 squares west of the small tree as indicated on the clue map.</td>
   </tr>
   <tr> 
-    <td><img src="img/clue_guides/varrock_lumber.jpg" style="width:200px;" alt="varrock_lumber.jpg"></td>
-    <td>Varrock Lumberyard Map - NorthEast from Draynor Bank</td>
-  </tr>
-  <tr> 
-    <td><img src="img/clue_guides/varrock_mine.jpg" style="width:200px;" alt="Pic missing, but coming soon!"></td>
-    <td>Varrock Mine Map - Dig at the fence at the Varrock East mining site</td>
-  </tr>
-<tr> 
     <td><img src="img/clue_guides/clue1.jpg" style="width:200px;" alt="clue1.jpg"></td>
     <td>In Yanille, south east corner</td>
-</tr>
+  </tr>
   <tr>
-<td><img src="img/clue_guides/clue_Rimmington_chemists.png" style="width:200px;" alt="Chemist's house"></td>
+    <td><img src="img/clue_guides/clue_Rimmington_chemists.png" style="width:200px;" alt="Chemist's house"></td>
     <td>West of the Chemist's house west of Rimmington (South of Falador below the Crafting Guild).</td>
   </tr>
   <tr>
-<td><img src="img/clue_guides/clue_hobgoblin_peninsula.png" style="width:200px;" alt="Hobgoblin peninsula"></td>
+    <td><img src="img/clue_guides/clue_hobgoblin_peninsula.png" style="width:200px;" alt="Hobgoblin peninsula"></td>
     <td>It's on the Hobgoblin peninsula that sticks out west of the Crafting Guild (below the Make over Mage - found directly west after exiting Falador by the south entrance).</td>
   </tr>
   <tr>
-<td><img src="img/clue_guides/clue_McGrubors_wood.png" style="width:200px;" alt="McGrubor's wood"></td>
+    <td><img src="img/clue_guides/clue_McGrubors_wood.png" style="width:200px;" alt="McGrubor's wood"></td>
     <td>Inside McGrubor's wood, which is west of Seer's village.</td>
   </tr>
-<tr>
-<td><img src="img/clue_guides/clue_clock_tower.jpg" style="width:200px;" alt="Pic missing, but coming soon!"></td>
+  <tr>
+    <td><img src="img/clue_guides/clue_clock_tower.jpg" style="width:200px;" alt="Pic missing, but coming soon!"></td>
     <td>Search a crate on the west side of the Clock Tower, which is south of Ardougne.</td>
   </tr>
-<tr>
-<td><img src="img/clue_guides/clue_west_ardougne.jpg" style="width:200px;" alt="clue_west_ardougne.jpg"></td>
+  <tr>
+    <td><img src="img/clue_guides/clue_west_ardougne.jpg" style="width:200px;" alt="clue_west_ardougne.jpg"></td>
     <td>Northwest corner of West Ardougne</td>
   </tr>
-<tr>
+  <tr>
     <td><img src="img/clue_guides/wiztowermap.gif" style="width:200px;" alt="wiztowermap.gif"></td>
     <td>Behind the Wizard's Tower</td>
-  </tr>
+  </tr>-->
+</table>
+<hr>
+<table class="calculators" width="100%">
   <tr>
     <th colspan=2>Castle Puzzle Box Solver</th>
   </tr>
   <tr> 
     <td colspan=2>
-    <canvas id="puzzleCanvasCastle" width="220" height="220"></canvas><br>
+    <canvas id="puzzleCanvasCastle" width="220" height="220"></canvas><br><br>
     <button id="castleSolveButton">Solve</button>
     <button id="castleNextButton">Next Move</button>
     <button id="castlePrevButton">Previous Move</button>
