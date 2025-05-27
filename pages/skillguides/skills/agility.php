c98db7ee5c5f9ed6dcf8c63d9713ada78b152144
halogod35 <hondadavey@gmail.com>
2025-05-27 16:49:43 -0400

    fix: corrected levels and added fallibility/hr breaks

--- pages/skillguides/skills/agility.php
+++ pages/skillguides/skills/agility.php
@@ -1,7 +1,7 @@
 <?php
 function getSkillContent($skill) { return <<<HTML
 <h2>$skill Skill Guide</h2>
-<br>
+<hr>
 <b><font color="Green">Gnomes' Course</font></b><br>
 <br>
 Located in the Gnomes' Stronghold North West of Ardougne, this is where you will have to start to train initially. You will see Gnome trainers around the course encouraging you to go faster and also ready to help you out with any questions.
@@ -18,61 +18,70 @@ Climb down this tree branch to get back to the ground.<br>
 Climb over this net, almost at the end now.<br>
 Crawl through these pipes to end up on the other side.<br>
 Congratulations on completing a whole lap! Heres how the xp works for this course.
-
 <br><br>
 <table class="calculators">
     <tr>
         <th>Level</th>
         <th>Obstacle</th>
         <th>EXP</th>
+        <th>Failable?</th>
     </tr>
     <tr>
         <td>1</td>
         <td>Log</td>
         <td>7.5</td>
+        <td>No</td>
     </tr>
     <tr>
         <td>1</td>
         <td>First Net</td>
         <td>7.5</td>
+        <td>No</td>
     </tr>
     <tr>
         <td>1</td>
         <td>First Tree Branch</td>
-        <td>5.0</td>
+        <td>5</td>
+        <td>No</td>
     </tr>
     <tr>
         <td>1</td>
         <td>Tight Rope</td>
         <td>7.5</td>
+        <td>No</td>
     </tr>
     <tr>
         <td>1</td>
         <td>Second Tree Branch</td>
-        <td>5.0</td>
+        <td>5</td>
+        <td>No</td>
     </tr>
     <tr>
         <td>1</td>
         <td>Second Net</td>
         <td>7.5</td>
+        <td>No</td>
     </tr>
     <tr>
         <td>1</td>
         <td>Pipes</td>
         <td>7.5</td>
+        <td>No</td>
     </tr>
     <tr>
         <td>1</td>
         <td>Completed Lap</td>
-        <td>86.0</td>
+        <td>86</td>
+        <td>No</td>
     </tr>
     <tr>
         <td>1</td>
         <td>Course Total</td>
         <td>133.5</td>
+        <td>No</td>
     </tr>
 </table>
-<br><br>
+<hr>
 <b><font color="Orange">Barbarians' Course</font></b>
 <br><br>
 Located right at the top of the World, you will have to go far North from Ardougne and past Baxtorian falls to access the Barbarian Outpost where this course is at. To enter the outpost, you must have completed the Bar Crawl. To then enter the agility course you must squeeze through the Pipe which requires level 35 agility.
@@ -95,49 +104,59 @@ Congratulations on completing a whole lap! Heres how the xp works for this cours
         <th>Level</th>
         <th>Obstacle</th>
         <th>EXP</th>
+        <th>Failable?</th>
     </tr>
     <tr>
         <td>35</td>
         <td>Pipe</td>
-        <td>10.0</td>
+        <td>10</td>
+        <td>No</td>
     </tr>
     <tr>
-        <td>35</td>
+        <td>1</td>
         <td>Rope Swing</td>
         <td>20</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
     <tr>
-        <td>35</td>
+        <td>1</td>
         <td>Log</td>
         <td>12.5</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
     <tr>
-        <td>35</td>
+        <td>1</td>
         <td>Net</td>
         <td>7.5</td>
+        <td>No</td>
     </tr>
     <tr>
-        <td>35</td>
+        <td>1</td>
         <td>Ledge</td>
-        <td>20.0</td>
+        <td>20</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
     <tr>
-        <td>35</td>
+        <td>1</td>
         <td>Crumbling Wall</td>
         <td>12.5</td>
+        <td>No</td>
     </tr>
     <tr>
-        <td>35</td>
+        <td>1</td>
         <td>Completed Lap</td>
         <td>139.5</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
     <tr>
-        <td>35</td>
+        <td>1</td>
         <td>Course Total</td>
         <td>222</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
 </table>
-<br><br>
+<p>Note: <i>The course itself has no Agility requirement, but accessing it requires 35 Agility to crawl through the entrance pipe.</i></p>
+<hr>
 <b><font color="Red">Wilderness Course</font></b>
 <br><br>
 Located deep in the members' area of the wilderness (level 52 and up) you will require level 52 agility to cross the initial ridge to gain access to this area. However sometimes you will still fail and fall into the pit of White Wolves. PKers do often come by to attack, so taking this risk is up to you - but the xp reward per lap is much great (look at the table below).
@@ -160,49 +179,58 @@ Congratulations on completing a whole lap! Heres how the xp works for this cours
         <th>Level</th>
         <th>Obstacle</th>
         <th>EXP</th>
+        <th>Failable?</th>
     </tr>
     <tr>
         <td>52</td>
         <td>Ridge</td>
-        <td>15.0</td>
+        <td>15</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
     <tr>
-        <td>52</td>
+        <td>49</td>
         <td>Pipe</td>
         <td>12.5</td>
+        <td>No</td>
     </tr>
     <tr>
-        <td>52</td>
+        <td>0</td>
         <td>Rope Swing</td>
-        <td>20.0</td>
+        <td>20</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
     <tr>
-        <td>52</td>
+        <td>0</td>
         <td>Stepping Stones</td>
-        <td>20.0</td>
+        <td>20</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
     <tr>
-        <td>52</td>
+        <td>0</td>
         <td>Log</td>
-        <td>20.0</td>
+        <td>20</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
     <tr>
-        <td>52</td>
+        <td>0</td>
         <td>Rocks</td>
         <td>0</td>
+        <td>No</td>
     </tr>
     <tr>
         <td>52</td>
         <td>Completed Lap</td>
         <td>571.5</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
     <tr>
         <td>52</td>
         <td>Course Total</td>
         <td>659</td>
+        <td><font color=orange>Yes</font></td>
     </tr>
 </table>
-<br><br>
+<hr>
 <b><font color="Yellow">Other Obstacles</font></b>
 <br><br>
 <table class="calculators">
@@ -215,7 +243,7 @@ Congratulations on completing a whole lap! Heres how the xp works for this cours
     <tr>
         <td>5</td>
         <td>Crumbling Wall</td>
-        <td>15.0</td>
+        <td>15</td>
         <td>This shortcut allows you to cross over from Taverly to Falador (but not back). This is located just behind West Falador bank.</td>
     </tr>
     <tr>
