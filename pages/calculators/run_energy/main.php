13a413e4115b2585bb8d3fb3bb64e43e54f97042
halogod35 <hondadavey@gmail.com>
2025-06-12 03:49:09 -0400

    fix: super minor run energy fixes

--- pages/calculators/run_energy/main.php
+++ pages/calculators/run_energy/main.php
@@ -9,12 +9,12 @@ function getCalcContent() { return <<<HTML
         <label>Agility Level:</label>
         <input type="number" id="agilityLevel" min="1" max="99" value="1">
         <label>Current Run (%):</label>
-        <input type="number" id="currentRunEnergy" min="0" max="100" value="10">
+        <input type="number" id="currentRunEnergy" min="0" max="100" value="0">
     </div>
     <hr>
     <h3>Time to Restore 1% Energy:<br><span id="restorePerPercent" class="gold-text"></span></h3>
     <h3>Time to Restore to 100%:<br><span id="restoreTime" class="gold-text"></span></h3>
-    <h3>~% Energy Restored per Minute:<span id="restoreOneMinute" class="gold-text"></span></h3>
+    <h3>~% Energy Restored per Minute: <span id="restoreOneMinute" class="gold-text"></span></h3>
     <br>
 </div>
 HTML.getJavaScriptVersion('js/calculators/run_energy.js'); }
