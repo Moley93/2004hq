2066f218c0d5daf5d5515f7025c0f920e9b7cbbd
halogod35 <hondadavey@gmail.com>
2025-06-27 15:45:15 -0400

    feat: added lightbox

--- js/main.js
+++ js/main.js
@@ -16,3 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+function openLightbox(src) {
+    const overlay = document.getElementById("lightbox");
+    const img = document.getElementById("lightbox-img");
+    img.src = src;
+    overlay.style.display = "flex";
+}
+  
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
--- pages/wallpapers/main.php
+++ pages/wallpapers/main.php
@@ -10,13 +10,16 @@ function getPageContent() {
     <h2>Wallpapers</h2>
     <p style="font-style:italic;">Bring Gielinor to your desktop with these official Jagex wallpapers</p>
     <hr>
+    <div class="lightbox-overlay" id="lightbox" onclick="closeLightbox()">
+        <img id="lightbox-img" src="">
+    </div>
     <!-- <h3>2004</h3> use later when we're in 2005 -->
     <table>
         <tbody>
             <tr>
                 <td>
                     <div><b>Warrior and Skeleton</b><br>
-                        <img src="img/rswallpapers/2004/w1_800x600.jpg" width="225"><br>
+                    <img src="img/rswallpapers/2004/w1_800x600.jpg" width="225" onclick="openLightbox(this.src)"><br>
                         <a href="img/rswallpapers/2004/w1_800x600.jpg" download>800x600</a><br>
                         <a href="img/rswallpapers/2004/w1_1024x768.jpg" download>1024x768</a><br>
                         <a href="img/rswallpapers/2004/w1_1280x1024.jpg" download>1280x1024</a><br>
@@ -26,7 +29,7 @@ function getPageContent() {
                 <td width="5"></td>
                 <td>
                     <div><b>Wandering Warrior</b><br>
-                        <img src="img/rswallpapers/2004/w2_800x600.jpg" width="225"><br>
+                        <img src="img/rswallpapers/2004/w2_800x600.jpg" width="225" onclick="openLightbox(this.src)"><br>
                         <a href="img/rswallpapers/2004/w2_800x600.jpg" download>800x600</a><br>
                         <a href="img/rswallpapers/2004/w2_1024x768.jpg" download>1024x768</a><br>
                         <a href="img/rswallpapers/2004/w2_1280x1024.jpg" download>1280x1024</a><br>
@@ -37,7 +40,7 @@ function getPageContent() {
             <tr>
                 <td style="padding-top: 2em;">
                     <div><b>The Grand Tree</b><br>
-                        <img src="img/rswallpapers/2004/w3_800x600.jpg" width="225"><br>
+                        <img src="img/rswallpapers/2004/w3_800x600.jpg" width="225" onclick="openLightbox(this.src)"><br>
                         <a href="img/rswallpapers/2004/w3_800x600.jpg" download>800x600</a><br>
                         <a href="img/rswallpapers/2004/w3_1024x768.jpg" download>1024x768</a><br>
                         <a href="img/rswallpapers/2004/w3_1280x1024.jpg" download>1280x1024</a><br>
@@ -47,7 +50,7 @@ function getPageContent() {
                 <td width="5"></td>
                 <td style="padding-top: 2em;">
                     <div><b>RuneScape Map</b><br>
-                        <img src="img/rswallpapers/2004/w4_800x600.jpg" width="225"><br>
+                        <img src="img/rswallpapers/2004/w4_800x600.jpg" width="225" onclick="openLightbox(this.src)"><br>
                         <a href="img/rswallpapers/2004/w4_800x600.jpg" download>800x600</a><br>
                         <a href="img/rswallpapers/2004/w4_1024x768.jpg" download>1024x768</a><br>
                         <a href="img/rswallpapers/2004/w4_1280x1024.jpg" download>1280x1024</a><br>
@@ -58,7 +61,7 @@ function getPageContent() {
             <tr>
                 <td colspan="3" style="padding-top: 2em;">
                     <div><b>Green Dragon</b><br>
-                        <img src="img/rswallpapers/2004/w5_800x600.jpg" width="225"><br>
+                        <img src="img/rswallpapers/2004/w5_800x600.jpg" width="225" onclick="openLightbox(this.src)"><br>
                         <a href="img/rswallpapers/2004/w5_800x600.jpg" download>800x600</a><br>
                         <a href="img/rswallpapers/2004/w5_1024x768.jpg" download>1024x768</a><br>
                         <a href="img/rswallpapers/2004/w5_1280x1024.jpg" download>1280x1024</a><br>
