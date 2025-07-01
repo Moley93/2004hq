6a1178cfea6456639ab02d7956d97cf98ed73eb8
halogod35 <hondadavey@gmail.com>
2025-06-30 20:32:19 -0400

    chore: changelog

--- pages/main/changelog.php
+++ pages/main/changelog.php
@@ -112,6 +112,7 @@ function getPageContent() {
     </tr>
   </thead>
   <tbody>
+    <tr><td>30/06/2025</td><td>Added miniquest section with barcrawl guide</td></tr>
     <tr><td>26/06/2025</td><td>Added wallpapers available for download</td></tr>
     <tr><td>23/05/2025</td><td>Major QoL improvements across all calculators</td></tr>
     <tr><td>10/05/2025</td><td>Clue tables now search across all tiers at once</td></tr>
