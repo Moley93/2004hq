08e53d8021052a2aafc533d8ae18133eeabc6268
halogod35 <hondadavey@gmail.com>
2025-07-28 21:17:15 -0400

    fix: more dragonslayer updates

--- pages/questguides/free/dragonslayer.php
+++ pages/questguides/free/dragonslayer.php
@@ -37,6 +37,8 @@ function getQuestGuide($questName, $questComplete) { return <<<HTML
 <br><br>
 <div data-progress>Talk to Oziach. Ask him about Rune platebody, the Dragonfire Shield, and the map pieces. He should give you a key to Melzar's Maze.</div>
 <br><br>
+<hr>
+<h3>Finding the Map Pieces</h3>
 <div data-progress>Go to the Oracle (located on Ice Mountain north of the Dwarven Mines) with your Silk, Mind Bomb, Unfired bowl, and Lobster pot. She'll tell you about a door in the mines. Go down into the mine and find the door on the eastern side. Inside, your items will disappear, and you'll find a chest containing a map piece.</div>
 <br><br>
 <div data-progress>Grab some food if needed. Head to Melzar's Maze (south of the Crafting Guild) and use your key on the door. Kill giant rats until one drops a red key. Use it on the northwest door, then go up the ladder.</div>
@@ -47,23 +49,32 @@ function getQuestGuide($questName, $questComplete) { return <<<HTML
 <br><br>
 <div data-progress>Kill zombies until one drops a blue key. Use it on the northwest door. Kill Melzar the Mad until he drops a magenta key. Use it to proceed.</div>
 <br><br>
-<div data-progress>Kill the Lesser Demon to get the final (green) key—thanks to graystar for this info! Use it on the door, then check the chest to get another map piece.</div>
+<div data-progress>Kill the Lesser Demon to get the final (green) key. The lesser demon can be safespotted standing near the western most door. Use the key on the door, then check the chest to get another map piece.</div>
 <br><br>
-<div data-progress>Stop by a bank. Choose one of the following: A) A bow and arrows, B) Runes to cast Telekinetic Grab, or C) 10,000 gp. If you brought the 10k, you don't need runes. If you're planning to kill Wormbrain, you'll need to telegrab the map piece.</div>
+<div data-progress>Stop by a bank. Choose one of the following: A) A bow and arrows/runes for combat spells and runes to cast Telekinetic Grab or B) 10,000 gp. If you brought the 10k, you don't need runes. If you're planning to kill Wormbrain, you'll need to telegrab the map piece.</div>
 <br><br>
-<div data-progress>Mages/Archers: Go to the Port Sarim jail and attack Wormbrain. When he dies, use Telekinetic Grab on the map piece. Use all map pieces on each other to form the full map.</div>
+<div data-progress>Mages/Archers: Go to the Port Sarim jail and attack Wormbrain. When he dies, use Telekinetic Grab on the map piece.</div>
+<br><br>
+<div data-progress>Use all map pieces on each other to form the full map.</div>
 <br><br>
 <div data-progress>If you can't telegrab: Talk to Wormbrain in the Port Sarim jail. Ask about the map piece. He'll eventually offer to sell it for 10k. Pay him, and you'll receive the map piece. Combine all pieces to form the full map.</div>
 <br><br>
+<hr>
+<h3>Repairing Lady Lumbridge and the Fight with Elvarg</h3>
 <div data-progress>Go to Lumbridge Castle and talk to the Duke. Ask about a shield to protect against dragons. He'll give you the Dragonfire Shield. It's advised to drop it, get another, and pick up the first one—just in case you die later.</div>
 <br><br>
-<div data-progress>Withdraw 2,000 coins, a hammer, 3 planks, 12 nails, and your map. Go to Port Sarim, then head to the dock east of the food store. Talk to the captain. After some dialogue, he'll offer to sell you the ship. Buy it, board the ship, and go below deck. Use your planks on the hole near you to repair it. Talk to the swabbie to disembark.</div>
+<div data-progress>Withdraw 2,000 coins, a hammer, 3 planks, 12 nails, and your map. Go to Port Sarim, then head to the far east end of the southernmost dock.</div>
+<br><br>
+<div data-progress>Talk to the captain. After some dialogue, he'll offer to sell you the ship. Buy it, board the ship, and go below deck. Use your planks on the hole near you to repair it.</div>
 <br><br>
 <div data-progress>Go to Draynor and talk to Ned. He'll offer to sail you to Crandor. Give him the map, and he'll tell you to meet him at the ship. Now's the time to prepare: bring potions, food, armor, weapons (magic or ranged), and most importantly—your Dragonfire Shield.</div>
 <br><br>
 <div data-progress>Head to your ship (The Lady Lumbridge), go below deck, and talk to Ned. Off to Crandor!</div>
 <br><br>
-<div data-progress>After arriving, the ship will be damaged. Disembark and climb the hill. Follow the path past King Scorpions. Evade them and walk north to another upward path. Go up, dodge the Lesser Demon, then enter the cave.</div>
+<div data-progress>After arriving, the ship will be damaged. Disembark and climb the hill.</div>
+<img src="img/questimages/dragonslayer2.png">
+<br><br>
+<div data-progress>Once you reach the top, enter the dungeon. Follow the path past King Scorpions. Evade them and walk north to another upward path. Go up, dodge the Lesser Demon, then enter the cave.</div>
 <br><br>
 <div data-progress>Inside, dodge the skeletons and walk deeper into the cave. Go through the door. This is a good time to drink any stat-boosting potions. If you forgot your Dragonfire Shield, now's the time to leave and retrieve it.</div>
 <br><br>
@@ -72,7 +83,7 @@ function getQuestGuide($questName, $questComplete) { return <<<HTML
 <div data-progress>After defeating Elvarg, you'll be teleported outside. If a skeleton attacks you, don't panic. Head south toward the Lesser Demons and look for a “secret” wall with an “Open” command. Your character will memorize this location for future access. From here, either teleport to Lumbridge or sell items at the general store to afford a trip off Karamja.</div>
 <br><br>
 Here is a picture of the wall:<br>
-<img src="img/questimages/dragonslayer1.png" height="400" width="400">
+<img src="img/questimages/dragonslayer1.png" width="450">
 <br><br>
 <div data-progress>Congratulations—you've completed Dragon Slayer! Go buy that Rune platebody, you legend.</div>
 $questComplete
