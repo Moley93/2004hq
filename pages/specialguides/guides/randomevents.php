526fe8b6eda0267cd3c7e257551057ee03e8883e
halogod35 <hondadavey@gmail.com>
2025-08-07 14:04:42 -0400

    fix: applied all geerin's reports/suggestions

--- pages/specialguides/guides/randomevents.php
+++ pages/specialguides/guides/randomevents.php
@@ -10,7 +10,7 @@ Below, all the different random events are explained with details of when they a
 General Random events are not skill limited, they can happen anywhere and anytime and are there to make sure you are awake and at the keyboard.
 <br><br>
 <h4>Mysterious Old Man</h4>
-<table><tr><td><img src="img/special_guides/randomevents/oldman.png"></td><td>The Mysterious Old Man (MOM) appears and interrupts whatever it is that you were doing. Some say he's just lonely, so if he talks to you, you should talk back. If you do, he'll reward you with a gift: possibly kebabs, spinach rolls, or caskets. If you ignore him and do not reply he will teleport you to a remote location and note your inventory.</td></tr></table>
+<table><tr><td><img src="img/special_guides/randomevents/oldman.png"></td><td>The Mysterious Old Man (MOM) appears and interrupts whatever it is that you were doing. Some say he's just lonely, so if he talks to you, you should talk back. If you do, he'll reward you with a gift: possibly kebabs, spinach rolls, or caskets. If you ignore him and do not reply he will teleport you to a remote location<!-- [in 225] and note your inventory-->.</td></tr></table>
 <br>
 Caskets have the following drops:
 <br>
@@ -41,7 +41,9 @@ Caskets have the following drops:
 <table><tr><td>A drunken dwarf, similiar to the old man will appear anywhere and give you a beer and a kebab. Talk to him straight away and recieve a kebab and a beer! If you ignore him he will start attacking you weakly with badly aimed rocks, you cannot attack him back. He will attack you indefinitely until you talk to him or climb a ladder/teleport.</td><td><img src="img/special_guides/randomevents/dwarf.png"></td></tr></table>
 <br>
 <h4>Genie</h4>
-<table><tr><td><img src="img/special_guides/randomevents/genie.png"></td><td>He appears and gives you a magic lamp, when you rub the magic lamp you get an options screen like the one below where you can get free xp for a any skill of your choice, the amount of xp you get if given below the picture: (Note: you cannot bank or trade the lamp) Answer him and recieve your lamp. If you don't, he will scatter your inventory across the floor or teleport you somewhere else.</td></tr></table>
+<table><tr><td><img src="img/special_guides/randomevents/genie.png"></td><td>He appears and gives you a magic lamp, when you rub the magic lamp you get an options screen like the one below where you can get free xp for a any skill of your choice, the amount of xp you get if given below the picture: (Note: you cannot bank or trade the lamp) Answer him and recieve your lamp. If you don't, <!-- [in 225] he will scatter your inventory across the floor or -->he will teleport you somewhere else.</td></tr></table>
+<br>
+Lamp Interface
 <br>
 <img src="img/special_guides/randomevents/geniechoice.png">
 <br>
@@ -51,7 +53,7 @@ XP given = Level in that skill * 10
 <table><tr><td>This annoying swarm will attack you hitting ticklish 1's and 2's while you are doing anything. You cannot attack them so run away until they stop chasing you - insect repellent doesn't work.</td><td><img src="img/special_guides/randomevents/swarm.png"></td></tr></table>
 <br>
 <h4>Strange Plant</h4>
-<table><tr><td><img src="img/special_guides/randomevents/strangeplant.png"></td><td>While doing anything a strangle plant will start growing next to you. It will be fully grown in about a minute or so. When its fully grown 'pick' the plant to get a 'strange fruit' which when eaten replenishes 10% of your energy, if you don't pick it, the plant will attack and could poison you.</td></tr></table>
+<table><tr><td><img src="img/special_guides/randomevents/strangeplant.png"></td><td>While doing anything a strangle plant will start growing next to you. It will be fully grown in about a minute or so. When its fully grown 'pick' the plant to get a 'strange fruit' which when eaten replenishes <!-- 10% [in 225] -->30% of your energy and cures poison. If you don't pick it, the plant will attack and could poison you.</td></tr></table>
 <hr>
 <h3>Mining Random Events</h3>
 <h4>Rock Golem</h4>
@@ -61,27 +63,27 @@ XP given = Level in that skill * 10
 <table><tr><td><img src="img/special_guides/randomevents/gasrock.png"></td><td>If you mine the rock too long it will eventually start smoking - if you hit it you will cause an explosions hitting you 10hp and breaking your pickaxe. Don't hit it!!! If you did, you can fix your pickaxe at the Pick Salesman Nurof (in the Dwarven Mine) for a small fee.</td></tr></table>
 <br>
 <h4>Pickaxe Splitting</h4>
-<table><tr><td>Your pickaxe head will fly off from its handle a few squares away. Pick it up and re-attach them by 'using' them together.</td><td><img src="img/special_guides/randomevents/pickaxe_head.png"></td></tr></table>
+<table><tr><td>Your pickaxe head will fly off from its handle a few squares away. Pick it up and re-attach it to the pickaxe handle by 'using' them together.</td><td><img src="img/special_guides/randomevents/pickaxe_head.png"></td></tr></table>
 <hr>
 <h3>Fishing Random Events</h3>
 <h4>River Troll</h4>
 <table><tr><td><img src="img/special_guides/randomevents/rivertroll.png"></td><td>The River Troll will attack you if you have been fishing on the same spot too long. Its level also varies according to yours. Varies between 14 - 170. Just run away until it stops chasing you - unless you want to kill it. It can drop bait, raw fish, oysters, or junk.</td></tr></table>
 <br>
 <h4>Big Fish</h4>
-<table><tr><td>This sneaky fish will appear and steal your fishing equipment dropping it on the floor next to you while it destroys the fishing spot for a while. Pick up your equiment and wait for the fishing spot to come back or move onto another spot.</td><td><img src="img/special_guides/randomevents/bigfish.png"></td></tr></table>
+<table><tr><td>This sneaky fish will appear to snatch your fishing equipment, sending it flying nearby, and temporarily disables the fishing spot for a while. Pick up your equipment and wait for the fishing spot to come back or move onto another spot.</td><td><img src="img/special_guides/randomevents/bigfish.png"></td></tr></table>
 <br>
 <h4>Whirlpool</h4>
 <table><tr><td><img src="img/special_guides/randomevents/whirlpool.png"></td><td>A whirlpool will engluf the fishing spot disabling you from fishing in it until it goes away minutes later. If you try to fish in the whirlpool, unless you are level 76 or above, you will lose your equipment. Wait for the spot to return or move to another spot.</td></tr></table>
 <hr>
 <h3>Woodcutting Random Events</h3>
 <h4>Tree Spirit</h4>
-<table><tr><td>The Tree Spirit will attack you if you have been woodcutting for some time, their levels vary according to yours; varies between 14 - 170. Just run away until it stops chasing you - unless you want to kill it. It can drop axes, bananas, and nature runes.</td><td><img src="img/special_guides/randomevents/treespirit.png"></td></tr></table>
+<table><tr><td>The Tree Spirit will attack you if you have been woodcutting for some time, their levels vary according to yours; varies between 14 - 170. Just run away until it stops chasing you - unless you want to kill it. It can drop nature runes or woodcutting axes from steel to rune. It is one of the two only NPCs that can drop a rune axe, with the other being the King Black Dragon.</td><td><img src="img/special_guides/randomevents/treespirit.png"></td></tr></table>
 <br>
 <h4>Ent</h4>
-<table><tr><td><img src="img/special_guides/randomevents/ent.png"></td><td>Sometime you will find your tree you are cutting has turned into an Ent, who will take your axes and break it. You can fix your axe at Bob's axes in Lumbridge, for a small fee.</td></tr></table>
+<table><tr><td><img src="img/special_guides/randomevents/ent.png"></td><td>Sometimes you will find your tree you are cutting has turned into an Ent. If you continue chopping the Ent, your axe will break. You can fix your axe at Bob's axes in Lumbridge, for a small fee.</td></tr></table>
 <br>
 <h4>Axe Splitting</h4>
-<table><tr><td>Your axe head will fly off a few squares away when you are woodcutting. Pick up the head and re-attach it to the shaft by 'using' them together</td><td><img src="img/wip.png"></td></tr></table>
+<table><tr><td>Your axe head will fly off a few squares away when you are woodcutting. Pick up the head and re-attach it to the axe handle by 'using' them together.</td><td><img src="img/wip.png"></td></tr></table>
 <hr>
 <h3>Thieving Random Events</h3>
 <h4>Watchman</h4>
